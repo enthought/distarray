a6e0f1b452142b2886b39a2afca50394f7425208
mwrobel <mwrobel@txcorp.com>
2008-07-03 17:36:04 -0600

    More tests in.

--- distarray/fft/tests/test_fftw.py
+++ distarray/fft/tests/test_fftw.py
@@ -18,7 +18,7 @@ class Test2dForward(unittest.TestCase):
     Is the __init__ method working properly?
     """
     
-    def test_basic(self):
+    def test_float64(self):
         """
         Test basic DistArray creation.
         """
@@ -35,8 +35,77 @@ class Test2dForward(unittest.TestCase):
                 pass
             else:
                 result = fftw.fft2(da)
-                numpy_result = np.fft.fft2(np.fromfunction(f,(16,16)))
-                (low, high) = result.global_corners()
+                numpy_result = np.fft.fft2(np.fromfunction(f,(16,16),dtype='float64'))
+                (low, high) = result.global_limits(0)
+                self.assert_(assert_array_almost_equal(result.local_view(), numpy_result[low:high,:], 2))
+                # Compare the part of numpy_result that this processor has with result.local_array
+                comm.Free()
+                
+    def test_float32(self):
+        """
+        Test basic DistArray creation.
+        """
+        try:
+            comm = create_comm_of_size(4)
+        except InvalidCommSizeError:
+            pass
+        else:
+            try:
+                def f(i,j):
+                    return i+j
+                da = densedistarray.fromfunction(f,(16,16),comm=comm,dtype='float32')
+            except NullCommError:
+                pass
+            else:
+                result = fftw.fft2(da)
+                numpy_result = np.fft.fft2(np.fromfunction(f,(16,16),dtype='float32'))
+                (low, high) = result.global_limits(0)
+                self.assert_(assert_array_almost_equal(result.local_view(), numpy_result[low:high,:], 2))
+                # Compare the part of numpy_result that this processor has with result.local_array
+                comm.Free()
+
+    def test_int16(self):
+        """
+        Test basic DistArray creation.
+        """
+        try:
+            comm = create_comm_of_size(4)
+        except InvalidCommSizeError:
+            pass
+        else:
+            try:
+                def f(i,j):
+                    return i+j
+                da = densedistarray.fromfunction(f,(16,16),comm=comm,dtype='int16')
+            except NullCommError:
+                pass
+            else:
+                result = fftw.fft2(da)
+                numpy_result = np.fft.fft2(np.fromfunction(f,(16,16),dtype='int16'))
+                (low, high) = result.global_limits(0)
+                self.assert_(assert_array_almost_equal(result.local_view(), numpy_result[low:high,:], 2))
+                # Compare the part of numpy_result that this processor has with result.local_array
+                comm.Free()
+
+    def test_complex128(self):
+        """
+        Test basic DistArray creation.
+        """
+        try:
+            comm = create_comm_of_size(4)
+        except InvalidCommSizeError:
+            pass
+        else:
+            try:
+                def f(i,j):
+                    return i+j
+                da = densedistarray.fromfunction(f,(16,16),comm=comm,dtype='complex128')
+            except NullCommError:
+                pass
+            else:
+                result = fftw.fft2(da)
+                numpy_result = np.fft.fft2(np.fromfunction(f,(16,16),dtype='complex128'))
+                (low, high) = result.global_limits(0)
                 self.assert_(assert_array_almost_equal(result.local_view(), numpy_result[low:high,:], 2))
                 # Compare the part of numpy_result that this processor has with result.local_array
                 comm.Free()
