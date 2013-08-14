78135cda52ae5e92679f2200726cc990a375df88
Robert David Grant <rgrant@enthought.com>
2013-08-14 14:47:51 -0500

    Add support for keyword arguments to decorator.

--- distarray/odin.py
+++ distarray/odin.py
@@ -3,6 +3,24 @@ ODIN: ODin Isn't Numpy
 """
 
 from distarray.client import DistArrayProxy
+from operator import add
+
+
+def flatten(lst):
+    """ Given a list of lists, return a flattened list.
+
+    Only flattens one level.  For example,
+
+    >>> flatten(zip(['a', 'b', 'c'], [1, 2, 3]))
+    ['a', 1, 'b', 2, 'c', 3]
+
+    >>> flatten([[1, 2], [3, 4, 5], [[5], [6], [7]]])
+    [1, 2, 3, 4, 5, [5], [6], [7]]
+    """
+    if len(lst) == 0:
+        return []
+    else:
+        return list(reduce(add, lst))
 
 
 def local(context):
@@ -23,23 +41,39 @@ def local(context):
         result_key = context._generate_key()
 
         def inner(*args, **kwargs):
-            keys = []
+            arg_keys = []
             for arg in args:
                 if isinstance(arg, DistArrayProxy):
                     # if a DistArrayProxy, use its existing key
-                    keys.append(arg.key)
+                    arg_keys.append(arg.key)
                     is_self = (context == arg.context)
                     err_msg_fmt = "distarray context mismatch: {} {}"
                     assert is_self, err_msg_fmt.format(context, arg.context)
                 else:
                     # if not a DistArrayProxy, key it and push it to engines
-                    keys.extend(context._key_and_push(arg))
+                    arg_keys.extend(context._key_and_push(arg))
+
+            kwarg_names = []
+            kwarg_keys = []
+            for kwarg_name, kwarg_value in kwargs.items():
+                kwarg_names.append(kwarg_name)
+                if isinstance(kwarg_value, DistArrayProxy):
+                    # if a DistArrayProxy, use its existing key
+                    kwarg_keys.append(kwarg_value.key)
+                    is_self = (context == kwarg_value.context)
+                    err_msg_fmt = "distarray context mismatch: {} {}"
+                    assert is_self, err_msg_fmt.format(context, arg.context)
+                else:
+                    # if not a DistArrayProxy, key it and push it to engines
+                    kwarg_keys.extend(context._key_and_push(kwarg_value))
 
             # build up a python statement as a string
             # then execute it, returning the result as a DistArrayProxy
-            args_fmt = ','.join(['{}'] * len(keys))
-            statement_fmt = '{} = {}(' + args_fmt + ')'
-            replacement_values = [result_key, func_key] + keys
+            args_fmt = ','.join(['{}'] * len(arg_keys))
+            kwargs_fmt = ','.join(['{}={}'] * len(kwarg_keys))
+            statement_fmt = '{} = {}(' + args_fmt + ',' + kwargs_fmt + ')'
+            replacement_values = ([result_key, func_key] + arg_keys +
+                                  flatten(zip(kwarg_names, kwarg_keys)))
             statement = statement_fmt.format(*replacement_values)
             context._execute(statement)
             return DistArrayProxy(result_key, context)
