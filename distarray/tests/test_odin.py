d795a16fcbb751044c4df99313854f817f574f54
Robert David Grant <rgrant@enthought.com>
2013-08-14 11:36:53 -0500

    Add local allclose, check values of tests.
    Before I was just checking if they run.

--- distarray/tests/test_odin.py
+++ distarray/tests/test_odin.py
@@ -16,6 +16,11 @@ dac = DistArrayContext(dv)
 
 
 @odin.local(dac)
+def assert_allclose(da, db):
+    assert np.allclose(da, db), "Arrays not equal within tolerance."
+
+
+@odin.local(dac)
 def local_sin(da):
     return np.sin(da)
 
@@ -64,29 +69,41 @@ class TestLocal(unittest.TestCase):
 
     def test_local_sin(self):
         db = local_sin(self.da)
+        assert_allclose(db, 0)
 
     def test_local_add(self):
         dc = local_add50(self.da)
+        assert_allclose(dc, 2 * np.pi + 50)
 
     def test_local_sum(self):
+        shape = self.da.get_localshapes()[0]
         dd = local_sum(self.da)
+        client_dd = np.array(dv.pull(dd.key))
+        sum_val = shape[0] * shape[1] * (2 * np.pi)
+        # return value is actually the sum of the whole distarrayproxy?!?
+        assert_allclose(client_dd, sum_val * len(self.da.get_localshapes()))
 
     def test_local_add_num(self):
         de = local_add_num(self.da, 11)
+        assert_allclose(de, 2 * np.pi + 11)
 
     def test_local_add_nums(self):
         df = local_add_nums(self.da, 11, 12, 13)
+        assert_allclose(df, 2 * np.pi + 11 + 12 + 13)
 
     def test_local_add_distarrayproxies(self):
         dg = dac.empty((1024, 1024))
         dg.fill(33)
         dh = local_add_distarrayproxies(self.da, dg)
+        assert_allclose(dh, 33 + 2 * np.pi)
 
     def test_local_add_mixed(self):
         di = dac.empty((1024, 1024))
         di.fill(33)
         dj = local_add_mixed(self.da, 11, di, 12)
+        assert_allclose(dj, 2 * np.pi + 11 + 33 + 12)
 
+    @unittest.skip('Locally adding ndarrays not supported.')
     def test_local_add_ndarray(self):
         shp = self.da.get_localshapes()[0]
         ndarr = np.empty(shp)
