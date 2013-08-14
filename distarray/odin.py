6c2fff26bb0a23e94eaa1891d34df197c13093e5
Robert David Grant <rgrant@enthought.com>
2013-08-14 14:57:56 -0500

    Factor keying and pushing out of decorator.

--- distarray/odin.py
+++ distarray/odin.py
@@ -23,6 +23,38 @@ def flatten(lst):
         return list(reduce(add, lst))
 
 
+def key_and_push_args(context, args):
+    arg_keys = []
+    for arg in args:
+        if isinstance(arg, DistArrayProxy):
+            # if a DistArrayProxy, use its existing key
+            arg_keys.append(arg.key)
+            is_self = (context == arg.context)
+            err_msg_fmt = "distarray context mismatch: {} {}"
+            assert is_self, err_msg_fmt.format(context, arg.context)
+        else:
+            # if not a DistArrayProxy, key it and push it to engines
+            arg_keys.extend(context._key_and_push(arg))
+    return arg_keys
+
+
+def key_and_push_kwargs(context, kwargs):
+    kwarg_names = []
+    kwarg_keys = []
+    for kwarg_name, kwarg_value in kwargs.items():
+        kwarg_names.append(kwarg_name)
+        if isinstance(kwarg_value, DistArrayProxy):
+            # if a DistArrayProxy, use its existing key
+            kwarg_keys.append(kwarg_value.key)
+            is_self = (context == kwarg_value.context)
+            err_msg_fmt = "distarray context mismatch: {} {}"
+            assert is_self, err_msg_fmt.format(context, kwarg_value.context)
+        else:
+            # if not a DistArrayProxy, key it and push it to engines
+            kwarg_keys.extend(context._key_and_push(kwarg_value))
+    return kwarg_names, kwarg_keys
+
+
 def local(context):
     """ Decorator indicating a function is run locally on engines.
 
@@ -41,40 +73,20 @@ def local(context):
         result_key = context._generate_key()
 
         def inner(*args, **kwargs):
-            arg_keys = []
-            for arg in args:
-                if isinstance(arg, DistArrayProxy):
-                    # if a DistArrayProxy, use its existing key
-                    arg_keys.append(arg.key)
-                    is_self = (context == arg.context)
-                    err_msg_fmt = "distarray context mismatch: {} {}"
-                    assert is_self, err_msg_fmt.format(context, arg.context)
-                else:
-                    # if not a DistArrayProxy, key it and push it to engines
-                    arg_keys.extend(context._key_and_push(arg))
-
-            kwarg_names = []
-            kwarg_keys = []
-            for kwarg_name, kwarg_value in kwargs.items():
-                kwarg_names.append(kwarg_name)
-                if isinstance(kwarg_value, DistArrayProxy):
-                    # if a DistArrayProxy, use its existing key
-                    kwarg_keys.append(kwarg_value.key)
-                    is_self = (context == kwarg_value.context)
-                    err_msg_fmt = "distarray context mismatch: {} {}"
-                    assert is_self, err_msg_fmt.format(context, arg.context)
-                else:
-                    # if not a DistArrayProxy, key it and push it to engines
-                    kwarg_keys.extend(context._key_and_push(kwarg_value))
+            # generate keys for each parameter
+            # push to engines if not a DistArrayProxy
+            arg_keys = key_and_push_args(context, args)
+            kwarg_names, kwarg_keys = key_and_push_kwargs(context, kwargs)
 
             # build up a python statement as a string
-            # then execute it, returning the result as a DistArrayProxy
             args_fmt = ','.join(['{}'] * len(arg_keys))
             kwargs_fmt = ','.join(['{}={}'] * len(kwarg_keys))
             statement_fmt = '{} = {}(' + args_fmt + ',' + kwargs_fmt + ')'
             replacement_values = ([result_key, func_key] + arg_keys +
                                   flatten(zip(kwarg_names, kwarg_keys)))
             statement = statement_fmt.format(*replacement_values)
+
+            # execute it locally and return the result as a DistArrayProxy
             context._execute(statement)
             return DistArrayProxy(result_key, context)
 
