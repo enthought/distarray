d3c5ac7058dfc2e30225dbfe768b7fcffbd8f845
Robert David Grant <rgrant@enthought.com>
2013-08-14 16:24:47 -0500

    Refactor @local decorator.
    @local no longer takes a context as an argument; a default context is set up when odin is imported.

--- distarray/tests/test_odin.py
+++ distarray/tests/test_odin.py
@@ -7,65 +7,61 @@ import unittest
 
 
 # To run these tests, you must have an ipcluster running
-# For example, `ipcluster start -n 4`
+# For example,
+# $ ipcluster start -n <n> --engines=MPIEngineSetLauncher
 
 
-c = Client()
-dv = c[:]
-dac = DistArrayContext(dv)
-
-
-@odin.local(dac)
+@odin.local
 def assert_allclose(da, db):
     assert np.allclose(da, db), "Arrays not equal within tolerance."
 
 
-@odin.local(dac)
+@odin.local
 def local_sin(da):
     return np.sin(da)
 
 
-@odin.local(dac)
+@odin.local
 def local_add50(da):
     return da + 50
 
 
-@odin.local(dac)
+@odin.local
 def local_sum(da):
     return np.sum(da)
 
 
-@odin.local(dac)
+@odin.local
 def local_add_num(da, num):
     return da + num
 
 
-@odin.local(dac)
+@odin.local
 def local_add_nums(da, num1, num2, num3):
     return da + num1 + num2 + num3
 
 
-@odin.local(dac)
+@odin.local
 def local_add_distarrayproxies(da, dg):
     return da + dg
 
 
-@odin.local(dac)
+@odin.local
 def local_add_mixed(da, num1, dg, num2):
     return da + num1 + dg + num2
 
 
-@odin.local(dac)
+@odin.local
 def local_add_ndarray(da, num, ndarr):
     return da + num + ndarr
 
 
-@odin.local(dac)
+@odin.local
 def local_add_kwargs(da, num1, num2=55):
     return da + num1 + num2
 
 
-@odin.local(dac)
+@odin.local
 def local_add_supermix(da, num1, db, num2, dc, num3=99, num4=66):
     return da + num1 + db + num2 + dc + num3 + num4
 
@@ -73,8 +69,8 @@ def local_add_supermix(da, num1, db, num2, dc, num3=99, num4=66):
 class TestLocal(unittest.TestCase):
 
     def setUp(self):
-        dv.execute('import numpy as np')
-        self.da = dac.empty((1024, 1024))
+        odin.view.execute('import numpy as np')
+        self.da = odin.context.empty((1024, 1024))
         self.da.fill(2 * np.pi)
 
     def test_local_sin(self):
@@ -88,7 +84,7 @@ class TestLocal(unittest.TestCase):
     def test_local_sum(self):
         shape = self.da.get_localshapes()[0]
         dd = local_sum(self.da)
-        client_dd = np.array(dv.pull(dd.key))
+        client_dd = np.array(odin.view.pull(dd.key))
         sum_val = shape[0] * shape[1] * (2 * np.pi)
         # return value is actually the sum of the whole distarrayproxy?!?
         assert_allclose(client_dd, sum_val * len(self.da.get_localshapes()))
@@ -102,13 +98,13 @@ class TestLocal(unittest.TestCase):
         assert_allclose(df, 2 * np.pi + 11 + 12 + 13)
 
     def test_local_add_distarrayproxies(self):
-        dg = dac.empty((1024, 1024))
+        dg = odin.context.empty((1024, 1024))
         dg.fill(33)
         dh = local_add_distarrayproxies(self.da, dg)
         assert_allclose(dh, 33 + 2 * np.pi)
 
     def test_local_add_mixed(self):
-        di = dac.empty((1024, 1024))
+        di = odin.context.empty((1024, 1024))
         di.fill(33)
         dj = local_add_mixed(self.da, 11, di, 12)
         assert_allclose(dj, 2 * np.pi + 11 + 33 + 12)
@@ -126,9 +122,9 @@ class TestLocal(unittest.TestCase):
         assert_allclose(dl, 2 * np.pi + 11 + 12)
 
     def test_local_add_supermix(self):
-        dm = dac.empty((1024, 1024))
+        dm = odin.context.empty((1024, 1024))
         dm.fill(22)
-        dn = dac.empty((1024, 1024))
+        dn = odin.context.empty((1024, 1024))
         dn.fill(44)
         do = local_add_supermix(self.da, 11, dm, 33, dc=dn, num3=55)
         expected = 2 * np.pi + 11 + 22 + 33 + 44 + 55 + 66
