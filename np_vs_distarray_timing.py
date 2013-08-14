a863b43fa328eded0ef6a4418fc01bd6a25f1797
Kurt Smith <ksmith@enthought.com>
2013-08-14 16:16:14 -0500

    Cleanup timings script.

--- np_vs_distarray_timing.py
+++ np_vs_distarray_timing.py
@@ -12,23 +12,30 @@ from timeit import timeit
 
 c = Client()
 dv = c[:]
-dv.execute('import os; os.chdir("%s")' % os.path.abspath(os.getcwd()))
 dac = DistArrayContext(dv)
 
-dist_timings = []
-np_timings = []
-repeat = 3
-for p in range(5, 10):
-    print p
-    N = 2**p
-    dist_a = dac.empty((N,))
-    reg_a = np.empty((N,))
-    dist_timings.append((N, timeit('dac.add(dist_a, dist_a)',
-                                   setup='from test_basic import dac, dist_a',
-                                   number=repeat) / float(repeat)))
-    np_timings.append((N, timeit('np.add(reg_a, reg_a)',
-                                 setup='from test_basic import np, reg_a',
-                                 number=repeat) / float(repeat)))
+def run_timings():
 
-np_timings = np.array(np_timings)
-dist_timings = np.array(dist_timings)
+    global dist_a, reg_a
+
+    dist_timings = []
+    np_timings = []
+    repeat = 3
+    for p in range(5, 10):
+        print p
+        N = 2**p
+        dist_a = dac.empty((N,))
+        reg_a = np.empty((N,))
+        dist_timings.append((N, timeit('dac.add(dist_a, dist_a)',
+                                    setup='from __main__ import dac, dist_a',
+                                    number=repeat) / float(repeat)))
+        np_timings.append((N, timeit('np.add(reg_a, reg_a)',
+                                    setup='from __main__ import np, reg_a',
+                                    number=repeat) / float(repeat)))
+
+    np_timings = np.array(np_timings)
+    dist_timings = np.array(dist_timings)
+    return np_timings, dist_timings
+
+if __name__ == '__main__':
+    np_timings, dist_timings = run_timings()
