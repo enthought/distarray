d187c51ccd9dc1676b6f16eddecee6dce752d668
Robert David Grant <rgrant@enthought.com>
2013-08-26 11:29:11 -0500

    Make class test-class name more specific
    ... to make room for more client tests.

--- distarray/tests/test_client.py
+++ distarray/tests/test_client.py
@@ -3,18 +3,18 @@ from IPython.parallel import Client
 from distarray.client import DistArrayContext
 
 
-class TestClient(unittest.TestCase):
+class TestDistArrayContext(unittest.TestCase):
 
     def setUp(self):
         self.client = Client()
         self.dv = self.client[:]
 
-    def testCreateDAC(self):
+    def test_create_DAC(self):
         '''Can we create a plain vanilla context?'''
         dac = DistArrayContext(self.dv)
         self.assertIs(dac.view, self.dv)
 
-    def testCreateDACwithTargets(self):
+    def test_create_DAC_with_targets(self):
         '''Can we create a context with a subset of engines?'''
         dac = DistArrayContext(self.dv, targets=[0, 1])
         self.assertIs(dac.view, self.dv)
