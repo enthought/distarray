cc3b56f5cc94aec3ef18ae0fefcb52d49ecad090
Robert David Grant <rgrant@enthought.com>
2013-08-28 14:40:22 -0500

    Make `test_utils` runnable.

--- distarray/tests/test_utils.py
+++ distarray/tests/test_utils.py
@@ -6,11 +6,16 @@ class TestMultPartitions(unittest.TestCase):
     """
     Test the multiplicative parition code.
     """
-    
+
     def test_both_methods(self):
         """
         Do the two methods of computing the multiplicative partitions agree?
         """
-        for s in [2,3]:
+        for s in [2, 3]:
             for n in range(2, 512):
-                self.assertEquals(utils.mult_partitions(n,s), utils.create_factors(n,s))
+                self.assertEquals(utils.mult_partitions(n, s),
+                                  utils.create_factors(n, s))
+
+
+if __name__ == '__main__':
+    unittest.main()
