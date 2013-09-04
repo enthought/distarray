d5170f01b6f2d6a118f725971a502a457f598315
Robert David Grant <rgrant@enthought.com>
2013-09-04 09:54:08 -0500

    Demo both block and cyclic dist types.
    Also, use pprint for prettiness.

--- distarray/tests/demo.py
+++ distarray/tests/demo.py
@@ -1,6 +1,7 @@
 import numpy as np
 import distarray
 from distarray import odin
+from pprint import pprint
 
 np.set_printoptions(precision=2, linewidth=1000)
 
@@ -34,30 +35,35 @@ if __name__ == '__main__':
 
     print
     raw_input("Basic creation:")
-    dap0 = odin.context.empty((arr_len,))
-    print "dap0 is a ", type(dap0)
+    dap_b = odin.context.empty((arr_len,), dist={0: 'b'})
+    dap_c = odin.context.empty((arr_len,), dist={0: 'c'})
+    print "dap_b is a ", type(dap_b)
+    print "dap_c is a ", type(dap_c)
 
     print
     raw_input("__setitem__:")
     for x in xrange(arr_len):
-        dap0[x] = x
-    print dap0.get_localarrays()
+        dap_b[x] = x
+        dap_c[x] = x
+    pprint(dap_b.get_localarrays())
+    pprint(dap_c.get_localarrays())
 
     print
     raw_input("__getitem__ with slicing:")
-    print dap0[30:40]
+    print dap_b[19:34:2]
+    print dap_c[19:34:2]
 
     print
     raw_input("@local functions:")
-    dap1 = local_sin(dap0)
-    print dap1.get_localarrays()
+    dap1 = local_sin(dap_b)
+    pprint(dap1.get_localarrays())
 
     print
     raw_input("calling @local functions from each other:")
-    dap2 = local_sin_plus_50(dap0)
-    print dap2.get_localarrays()
+    dap2 = local_sin_plus_50(dap_b)
+    pprint(dap2.get_localarrays())
 
     print
     raw_input("calling MPI from @local functions:")
-    dap3 = global_sum(dap0)
-    print dap3.get_localarrays()
+    dap3 = global_sum(dap_b)
+    pprint(dap3.get_localarrays())
