7b397470e5428c4e5be97377a1891189ca0b0390
Robert David Grant <rgrant@enthought.com>
2013-09-11 17:16:03 -0500

    Remove old IPython-parallel-based examples.
    One of these also uses `distarray.fft`.

--- docs/examples/interact.py
+++
@@ -1,8 +0,0 @@
-from IPython.kernel import client
-from distarray import client as daclient
-import numpy as np
-import distarray as da
-mec = client.MultiEngineClient()
-mec.execute('import distarray as da')
-dac = daclient.DistArrayContext(mec)
-mec.activate()
