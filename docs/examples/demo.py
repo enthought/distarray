c6e9889effb8be3b0ab0a5d59411a0ba8c7f2946
Robert David Grant <rgrant@enthought.com>
2013-10-02 17:59:55 -0500

    Make demo run under Python 3.3

--- docs/examples/demo.py
+++ docs/examples/demo.py
@@ -1,12 +1,13 @@
+from __future__ import print_function
 import numpy
 import distarray
+from six.moves import input
 from distarray import odin
 from pprint import pprint
 
-numpy.set_printoptions(precision=2, linewidth=1000)
 
-with odin.context.view.sync_imports():
-    import numpy
+numpy.set_printoptions(precision=2, linewidth=1000)
+odin.context.view.execute("import numpy")
 
 
 @odin.local
@@ -37,15 +38,15 @@ if __name__ == '__main__':
 
     arr_len = 40
 
-    print
-    raw_input("Basic creation:")
+    print()
+    input("Basic creation:")
     dap_b = odin.context.empty((arr_len,), dist={0: 'b'})
     dap_c = odin.context.empty((arr_len,), dist={0: 'c'})
-    print "dap_b is a ", type(dap_b)
-    print "dap_c is a ", type(dap_c)
+    print("dap_b is a ", type(dap_b))
+    print("dap_c is a ", type(dap_c))
 
-    print
-    raw_input("__setitem__:")
+    print()
+    input("__setitem__:")
     for x in range(arr_len):
         dap_b[x] = x
         dap_c[x] = x
@@ -53,21 +54,21 @@ if __name__ == '__main__':
     pprint(dap_c.get_localarrays())
 
 #    print
-#    raw_input("__getitem__ with slicing:")
+#    input("__getitem__ with slicing:")
 #    print dap_b[19:34:2]
 #    print dap_c[19:34:2]
 
-    print
-    raw_input("@local functions:")
+    print()
+    input("@local functions:")
     dap1 = local_sin(dap_b)
     pprint(dap1.get_localarrays())
 
-    print
-    raw_input("calling @local functions from each other:")
+    print()
+    input("calling @local functions from each other:")
     dap2 = local_sin_plus_50(dap_b)
     pprint(dap2.get_localarrays())
 
-    print
-    raw_input("calling MPI from @local functions:")
+    print()
+    input("calling MPI from @local functions:")
     dap3 = global_sum(dap_b)
     pprint(dap3.get_localarrays())
