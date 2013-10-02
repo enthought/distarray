6659fdebbc383d22c3abd303c41dbb0f326c12b1
Robert David Grant <rgrant@enthought.com>
2013-10-02 17:00:37 -0500

    Replace assertEquals with assertEquals for Py3.

--- distarray/tests/test_utils.py
+++ distarray/tests/test_utils.py
@@ -13,8 +13,8 @@ class TestMultPartitions(unittest.TestCase):
         """
         for s in [2, 3]:
             for n in range(2, 512):
-                self.assertEquals(utils.mult_partitions(n, s),
-                                  utils.create_factors(n, s))
+                self.assertEqual(utils.mult_partitions(n, s),
+                                 utils.create_factors(n, s))
 
 
 if __name__ == '__main__':
