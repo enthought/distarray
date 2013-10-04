eac60af6f63b2a7e57ecd54bb14651798841b66c
Robert David Grant <rgrant@enthought.com>
2013-10-04 16:55:40 -0500

    Add a script to bring up an ipcluster before testing.

---
+++ distarray/tests/run_ipcluster.py
@@ -0,0 +1,19 @@
+import six
+from subprocess import Popen, PIPE
+
+
+def run_ipcluster(n=4):
+    if six.PY2:
+        cmd = 'ipcluster'
+    elif six.PY3:
+        cmd = 'ipcluster3'
+    else:
+        raise NotImplementedError("Not run with Python 2 *or* 3?")
+
+    engines = "--engines=MPIEngineSetLauncher"
+    return Popen([cmd, 'start', '-n', str(n), engines, str('&')],
+                  stdout=PIPE, stderr=PIPE)
+
+
+if __name__ == '__main__':
+    run_ipcluster()
