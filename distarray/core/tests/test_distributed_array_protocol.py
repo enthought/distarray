7c39447c405d87a9baebdaba56c524a6757d613b
Robert David Grant <rgrant@enthought.com>
2013-10-09 18:01:04 -0500

    Have basic DAP tests pass on `NullCommError`.

--- distarray/core/tests/test_distributed_array_protocol.py
+++ distarray/core/tests/test_distributed_array_protocol.py
@@ -1,22 +1,26 @@
 import unittest
 import distarray as da
 from distarray.mpi.mpibase import create_comm_of_size, InvalidCommSizeError
+from distarray.core.error import NullCommError
 
 
 class TestDistributedArrayProtocol(unittest.TestCase):
 
     def setUp(self):
         try:
-            comm = create_comm_of_size(4)
+            self.comm = create_comm_of_size(4)
         except InvalidCommSizeError:
             raise unittest.SkipTest('Must run with comm size > 4.')
-        else:
-            self.arr = da.LocalArray((16,16),
-                                     grid_shape=(4,),
-                                     comm=comm, buf=None, offset=0)
 
     def test_export(self):
-        self.assertIsInstance(self.arr, da.LocalArray)
+        try:
+            self.arr = da.LocalArray((16,16),
+                                     grid_shape=(4,),
+                                     comm=self.comm, buf=None, offset=0)
+        except NullCommError:
+            pass
+        else:
+            self.assertIsInstance(self.arr, da.LocalArray)
 
 
 if __name__ == '__main__':
