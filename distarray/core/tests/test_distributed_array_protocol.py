32dc151b0aca5077e2b5b029bb9af5884da99328
Robert David Grant <rgrant@enthought.com>
2013-10-09 15:43:41 -0500

    Add extensive tests to DAP roundtrip test.

--- distarray/core/tests/test_distributed_array_protocol.py
+++ distarray/core/tests/test_distributed_array_protocol.py
@@ -1,19 +1,20 @@
 import unittest
 import distarray as da
 from distarray.mpi.mpibase import create_comm_of_size, InvalidCommSizeError
+from numpy.testing import assert_almost_equal
 
 
 class TestDistributedArrayProtocol(unittest.TestCase):
 
     def setUp(self):
         try:
-            comm = create_comm_of_size(4)
+            self.comm = create_comm_of_size(4)
         except InvalidCommSizeError:
             raise unittest.SkipTest('Must run with comm size > 4.')
         else:
             self.larr = da.LocalArray((16,16),
                                      grid_shape=(4,),
-                                     comm=comm, buf=None, offset=0)
+                                     comm=self.comm, buf=None, offset=0)
 
     def test_has_export(self):
         self.assertTrue(hasattr(self.larr, '__distarray__'))
@@ -57,11 +58,26 @@ class TestDistributedArrayProtocol(unittest.TestCase):
             self.assertIsInstance(dd['blocksize'], int)
             self.assertEqual(len(dd['padding']), 2)
 
-
-    @unittest.skip("Import not yet implemented.")
     def test_round_trip(self):
-        new_larr = da.fromdap(self.larr)
-        self.assertIs(new_larr.local_array, self.larr.local_array)
+        larr = da.fromdap(self.larr, comm=self.comm)
+        self.assertEqual(larr.shape, self.larr.shape)
+        self.assertEqual(larr.dist, self.larr.dist)
+        self.assertEqual(larr.grid_shape, self.larr.grid_shape)
+        self.assertEqual(larr.comm_size, self.larr.comm_size)
+        self.assertEqual(larr.ndistdim, self.larr.ndistdim)
+        self.assertEqual(larr.distdims, self.larr.distdims)
+        self.assertEqual(larr.map_classes, self.larr.map_classes)
+        self.assertEqual(larr.comm.Get_topo(), self.larr.comm.Get_topo())
+        self.assertEqual(len(larr.maps), len(self.larr.maps))
+        self.assertEqual(larr.maps[0].local_shape,
+                         self.larr.maps[0].local_shape)
+        self.assertEqual(larr.maps[0].shape, self.larr.maps[0].shape)
+        self.assertEqual(larr.maps[0].grid_shape, self.larr.maps[0].grid_shape)
+        self.assertEqual(larr.local_shape, self.larr.local_shape)
+        self.assertEqual(larr.local_array.shape, self.larr.local_array.shape)
+        self.assertEqual(larr.local_array.dtype, self.larr.local_array.dtype)
+        assert_almost_equal(larr.local_array, self.larr.local_array)
+        self.assertIs(larr.local_array.data, self.larr.local_array.data)
 
 
 if __name__ == '__main__':
