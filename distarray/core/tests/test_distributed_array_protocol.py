f555641e3642a9cded5d5f04daf42ea093867b9e
Robert David Grant <rgrant@enthought.com>
2013-10-10 13:19:22 -0500

    Use `comm_null_passes` decorator.

--- distarray/core/tests/test_distributed_array_protocol.py
+++ distarray/core/tests/test_distributed_array_protocol.py
@@ -1,7 +1,9 @@
 import unittest
 import distarray as da
-from distarray.mpi.mpibase import create_comm_of_size, InvalidCommSizeError
 from numpy.testing import assert_array_equal
+from distarray.mpi.mpibase import (MPI, create_comm_of_size,
+                                   InvalidCommSizeError)
+from distarray.utils import comm_null_passes
 
 
 class TestDistributedArrayProtocol(unittest.TestCase):
@@ -12,30 +14,36 @@ class TestDistributedArrayProtocol(unittest.TestCase):
         except InvalidCommSizeError:
             raise unittest.SkipTest('Must run with comm size > 4.')
         else:
-            self.larr = da.LocalArray((16,16),
-                                     grid_shape=(4,),
-                                     comm=self.comm, buf=None, offset=0)
+            if self.comm != MPI.COMM_NULL:
+                self.larr = da.LocalArray((16,16),
+                                         grid_shape=(4,),
+                                         comm=self.comm, buf=None, offset=0)
 
+    @comm_null_passes
     def test_has_export(self):
         self.assertTrue(hasattr(self.larr, '__distarray__'))
 
+    @comm_null_passes
     def test_export_keys(self):
         required_keys = set(("buffer", "dimdata"))
         export_data = self.larr.__distarray__()
         exported_keys = set(export_data.keys())
         self.assertEqual(required_keys, exported_keys)
 
+    @comm_null_passes
     def test_export_buffer(self):
         """See if we actually export a buffer."""
         export_data = self.larr.__distarray__()
         memoryview(export_data['buffer'])
 
+    @comm_null_passes
     def test_export_dimdata_len(self):
         """Test if there is a `dimdict` for every dimension."""
         export_data = self.larr.__distarray__()
         dimdata = export_data['dimdata']
         self.assertEqual(len(dimdata), self.larr.ndim)
 
+    @comm_null_passes
     def test_export_dimdata_keys(self):
         export_data = self.larr.__distarray__()
         dimdata = export_data['dimdata']
@@ -44,6 +52,7 @@ class TestDistributedArrayProtocol(unittest.TestCase):
         for dimdict in dimdata:
             self.assertEqual(required_keys, set(dimdict.keys()))
 
+    @comm_null_passes
     def test_export_dimdata_values(self):
         export_data = self.larr.__distarray__()
         dimdata = export_data['dimdata']
@@ -58,6 +67,7 @@ class TestDistributedArrayProtocol(unittest.TestCase):
             self.assertIsInstance(dd['blocksize'], int)
             self.assertEqual(len(dd['padding']), 2)
 
+    @comm_null_passes
     def test_round_trip_equality(self):
         larr = da.fromdap(self.larr, comm=self.comm)
         self.assertEqual(larr.shape, self.larr.shape)
@@ -78,6 +88,7 @@ class TestDistributedArrayProtocol(unittest.TestCase):
         self.assertEqual(larr.local_array.dtype, self.larr.local_array.dtype)
         assert_array_equal(larr.local_array, self.larr.local_array)
 
+    @comm_null_passes
     def test_round_trip_identity(self):
         larr = da.fromdap(self.larr, comm=self.comm)
         larr.local_array[0,0] = 99
