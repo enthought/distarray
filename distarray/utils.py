9c8d0e244dd76d4078e78200954552f5a2d40b60
Robert David Grant <rgrant@enthought.com>
2013-10-22 12:47:28 -0500

    Trim whitespace in utils.py.

--- distarray/utils.py
+++ distarray/utils.py
@@ -10,6 +10,7 @@ def divisors(n):
             yield i
         i += 1
 
+
 def multi_for(iterables):
     if not iterables:
         yield ()
@@ -17,6 +18,7 @@ def multi_for(iterables):
         for item in iterables[0]:
             for rest_tuple in multi_for(iterables[1:]):
                 yield (item,) + rest_tuple
+
 
 def create_factors(n, size=2):
     divs = list(divisors(n))
@@ -42,8 +44,10 @@ def divisors_minmax(n, dmin, dmax):
             yield i
         i += 1
 
+
 def list_or_tuple(seq):
     return isinstance(seq, (list, tuple))
+
 
 def flatten(seq, to_expand=list_or_tuple):
     """Flatten a nested sequence."""
@@ -54,15 +58,17 @@ def flatten(seq, to_expand=list_or_tuple):
         else:
             yield item
 
+
 def mult_partitions(n, s):
     """Compute the multiplicative partitions of n of size s
-    
+
     >>> mult_partitions(52,3)
     [(2, 2, 13)]
     >>> mult_partitions(52,2)
     [(2, 26), (4, 13)]
     """
     return [tuple(flatten(p)) for p in mult_partitions_recurs(n,s)]
+
 
 def mult_partitions_recurs(n, s, pd=1):
     if s == 1:
@@ -77,7 +83,7 @@ def mult_partitions_recurs(n, s, pd=1):
 
 def mirror_sort(seq, ref_seq):
     """Sort s2 into the order that s1 is in.
-    
+
     >>> mirror_sort(range(5),[1,5,2,4,3])
     [0, 4, 1, 3, 2]
     """
@@ -90,19 +96,22 @@ def mirror_sort(seq, ref_seq):
         newseq[shift[s_index]] = seq[s_index]
     return newseq
 
+
 def outer_zip(seqa, seqb):
     """An outer product, but using zip rather than *.
-    
+
     >>> a = 2*range(4)
     >>> b = range(8)
     >>> outer_zip(a,b)
     [[(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7)], [(1, 0), (1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (1, 7)], [(2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (2, 7)], [(3, 0), (3, 1), (3, 2), (3, 3), (3, 4), (3, 5), (3, 6), (3, 7)], [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7)], [(1, 0), (1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (1, 7)], [(2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (2, 7)], [(3, 0), (3, 1), (3, 2), (3, 3), (3, 4), (3, 5), (3, 6), (3, 7)]]
-    
+
     """
     return [[(i,j) for j in seqb] for i in seqa]
 
+
 def _raise_nie():
     raise NotImplementedError("This has not yet been implemented for distributed arrays")
+
 
 def comm_null_passes(fn):
     """Decorator. If `self.comm` is COMM_NULL, pass."""
