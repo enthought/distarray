1114119bc9b4878d12048345da9d2bfba6c57ce4
Robert David Grant <rgrant@enthought.com>
2013-10-30 15:54:39 -0500

    Refactor paralleltest_functions.

--- distarray/core/tests/paralleltest_functions.py
+++ distarray/core/tests/paralleltest_functions.py
@@ -1,80 +1,45 @@
 import unittest
 import numpy as np
 
-from distarray.mpi.error import InvalidCommSizeError
-from distarray.core.error import NullCommError
-from distarray.mpi.mpibase import create_comm_of_size
-from distarray.core import denselocalarray
+import distarray.core.denselocalarray as dla
+from distarray.testing import MpiTestCase, comm_null_passes
 
 
-class TestFunctions(unittest.TestCase):
+class TestFunctions(MpiTestCase):
 
+    @comm_null_passes
     def test_arecompatible(self):
-        """
-        Test if two DistArrays are compatible.
-        """
-        try:
-            comm = create_comm_of_size(4)
-        except InvalidCommSizeError:
-            raise unittest.SkipTest("Skipped due to Invalid Comm Size")
-        else:
-            try:
-                a = denselocalarray.LocalArray((16,16), dtype='int64', comm=comm)
-                b = denselocalarray.LocalArray((16,16), dtype='float32', comm=comm)
-            except NullCommError:
-                pass
-            else:
-                self.assertEqual(denselocalarray.arecompatible(a,b), True)
-                a = denselocalarray.LocalArray((16,16), dtype='int64', dist='c', comm=comm)
-                b = denselocalarray.LocalArray((16,16), dtype='float32', dist='b', comm=comm)
-                self.assertEqual(denselocalarray.arecompatible(a,b), False)
-                comm.Free()
+        """Test if two DistArrays are compatible."""
+        a = dla.LocalArray((16,16), dtype='int64', comm=self.comm)
+        b = dla.LocalArray((16,16), dtype='float32', comm=self.comm)
+        self.assertEqual(dla.arecompatible(a,b), True)
+        a = dla.LocalArray((16,16), dtype='int64', dist='c', comm=self.comm)
+        b = dla.LocalArray((16,16), dtype='float32', dist='b', comm=self.comm)
+        self.assertEqual(dla.arecompatible(a,b), False)
 
+    @comm_null_passes
     def test_fromfunction(self):
-        """
-        Can we build an array using fromfunction and a trivial function.
-        """
+        """Can we build an array using fromfunction and a trivial function?"""
         def f(*global_inds):
             return 1.0
 
-        try:
-            comm = create_comm_of_size(4)
-        except InvalidCommSizeError:
-            raise unittest.SkipTest("Skipped due to Invalid Comm Size")
-        else:
-            try:
-                a = denselocalarray.fromfunction(f, (16,16), dtype='int64', dist=('b','c'), comm=comm)
-            except NullCommError:
-                pass
-            else:
-                self.assertEqual(a.shape, (16,16))
-                self.assertEqual(a.dtype, np.dtype('int64'))
-                for global_inds, value in denselocalarray.ndenumerate(a):
-                    self.assertEqual(1.0, value)
-                comm.Free()
+        a = dla.fromfunction(f, (16,16), dtype='int64', dist=('b','c'), comm=self.comm)
+        self.assertEqual(a.shape, (16,16))
+        self.assertEqual(a.dtype, np.dtype('int64'))
+        for global_inds, value in dla.ndenumerate(a):
+            self.assertEqual(1.0, value)
 
+    @comm_null_passes
     def test_fromfunction_complicated(self):
-        """
-        Can we build an array using fromfunction and a nontrivial function.
-        """
+        """Can we build an array using fromfunction and a nontrivial function."""
         def f(*global_inds):
             return sum(global_inds)
 
-        try:
-            comm = create_comm_of_size(4)
-        except InvalidCommSizeError:
-            raise unittest.SkipTest("Skipped due to Invalid Comm Size")
-        else:
-            try:
-                a = denselocalarray.fromfunction(f, (16,16), dtype='int64', dist=('b','c'), comm=comm)
-            except NullCommError:
-                pass
-            else:
-                self.assertEqual(a.shape, (16,16))
-                self.assertEqual(a.dtype, np.dtype('int64'))
-                for global_inds, value in denselocalarray.ndenumerate(a):
-                    self.assertEqual(sum(global_inds), value)
-                comm.Free()
+        a = dla.fromfunction(f, (16,16), dtype='int64', dist=('b','c'), comm=self.comm)
+        self.assertEqual(a.shape, (16,16))
+        self.assertEqual(a.dtype, np.dtype('int64'))
+        for global_inds, value in dla.ndenumerate(a):
+            self.assertEqual(sum(global_inds), value)
 
 
 if __name__ == '__main__':
