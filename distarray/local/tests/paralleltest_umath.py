94b6624932847c37810ec464680456bedd69e1eb
Blake Griffith <blake.a.griffith@gmail.com>
2014-01-29 20:52:21 -0600

    Test boolean comparisons.
    This also corrected a lot of tests that were being missed, and pep8'd
    the files I was working in.

--- distarray/local/tests/paralleltest_umath.py
+++ distarray/local/tests/paralleltest_umath.py
@@ -1,3 +1,4 @@
+import warnings
 import unittest
 import numpy as np
 from numpy.testing import assert_array_equal
@@ -14,18 +15,20 @@ class TestUnaryUFunc(MpiTestCase):
     @comm_null_passes
     def test_negative(self):
         """See if unary ufunc works for a LocalArray."""
-        a = denselocalarray.LocalArray((16,16), dtype='int32', comm=self.comm)
+        a = denselocalarray.LocalArray((16, 16), dtype='int32', comm=self.comm)
         a.fill(1)
         b = denselocalarray.negative(a)
-        self.assertTrue(np.all(a.local_array==-b.local_array))
+        self.assertTrue(np.all(a.local_array == -b.local_array))
 
         b = denselocalarray.empty_like(a)
         b = denselocalarray.negative(a, b)
-        self.assertTrue(np.all(a.local_array==-b.local_array))
+        self.assertTrue(np.all(a.local_array == -b.local_array))
 
-        a = denselocalarray.LocalArray((16,16), dtype='int32', comm=self.comm)
-        b = denselocalarray.LocalArray((20,20), dtype='int32', comm=self.comm)
-        self.assertRaises(IncompatibleArrayError, denselocalarray.negative, b, a)
+        a = denselocalarray.LocalArray((16, 16), dtype='int32', comm=self.comm)
+        b = denselocalarray.LocalArray((20, 20), dtype='int32', comm=self.comm)
+        self.assertRaises(IncompatibleArrayError,
+                          denselocalarray.negative,
+                          b, a)
 
 
 class TestBinaryUFunc(MpiTestCase):
@@ -33,28 +36,28 @@ class TestBinaryUFunc(MpiTestCase):
     @comm_null_passes
     def test_add(self):
         """See if binary ufunc works for a LocalArray."""
-        a = denselocalarray.LocalArray((16,16), dtype='int32', comm=self.comm)
-        b = denselocalarray.LocalArray((16,16), dtype='int32', comm=self.comm)
+        a = denselocalarray.LocalArray((16, 16), dtype='int32', comm=self.comm)
+        b = denselocalarray.LocalArray((16, 16), dtype='int32', comm=self.comm)
         a.fill(1)
         b.fill(1)
         c = denselocalarray.add(a, b)
-        self.assertTrue(np.all(c.local_array==2))
+        self.assertTrue(np.all(c.local_array == 2))
 
         c = denselocalarray.empty_like(a)
         c = denselocalarray.add(a, b, c)
-        self.assertTrue(np.all(c.local_array==2))
+        self.assertTrue(np.all(c.local_array == 2))
 
-        a = denselocalarray.LocalArray((16,16), dtype='int32', comm=self.comm)
-        b = denselocalarray.LocalArray((20,20), dtype='int32', comm=self.comm)
+        a = denselocalarray.LocalArray((16, 16), dtype='int32', comm=self.comm)
+        b = denselocalarray.LocalArray((20, 20), dtype='int32', comm=self.comm)
         self.assertRaises(IncompatibleArrayError, denselocalarray.add, a, b)
 
-        a = denselocalarray.LocalArray((16,16), dtype='int32', comm=self.comm)
-        b = denselocalarray.LocalArray((16,16), dtype='int32', comm=self.comm)
-        c = denselocalarray.LocalArray((20,20), dtype='int32', comm=self.comm)
+        a = denselocalarray.LocalArray((16, 16), dtype='int32', comm=self.comm)
+        b = denselocalarray.LocalArray((16, 16), dtype='int32', comm=self.comm)
+        c = denselocalarray.LocalArray((20, 20), dtype='int32', comm=self.comm)
         self.assertRaises(IncompatibleArrayError, denselocalarray.add, a, b, c)
 
 
-def add_checkers(cls, ops):
+def add_checkers(cls, ops, bad_ops):
     """Add a test method to `cls` for all `ops`
 
     Parameters
@@ -62,11 +65,21 @@ def add_checkers(cls, ops):
     cls : a Test class
     ops : an iterable of functions
         Functions to check with self.check_op(self, op)
+    bad_ops : an iterable of functions that are in ops which should be
+        skipped.
     """
+    msg = ("This operation does not work with the default "
+           "testing data.")
+
+    def check(op):
+        return lambda self: self.check_op(op)
+
     for op in ops:
         fn_name = "test_" + op.__name__
-        fn_value = lambda self: self.check_op(op)
-        setattr(cls, fn_name, fn_value)
+        if op in bad_ops:
+            setattr(cls, fn_name, lambda _: unittest.skip(msg))
+        else:
+            setattr(cls, fn_name, check(op))
 
 
 class TestLocalArrayUnaryOperations(MpiTestCase):
@@ -78,18 +91,13 @@ class TestLocalArrayUnaryOperations(MpiTestCase):
         Check the one- and two-arg ufunc versions as well as the method
         version attached to a LocalArray.
         """
-        x = da.ones((16,16), dist=('b',None), comm=self.comm)
-        y = da.ones((16,16), dist=('b',None), comm=self.comm)
-        result0 = op(x)  # standard form
-        op(x, y=y)  # two-arg form
+        x = da.ones((16, 16), dist=('b', None), comm=self.comm)
+        y = da.ones((16, 16), dist=('b', None), comm=self.comm)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=RuntimeWarning)
+            result0 = op(x, casting='unsafe')  # standard form
+            op(x, y=y, casting='unsafe')  # two-arg form
         assert_array_equal(result0.local_array, y.local_array)
-
-uops = (dc.absolute, dc.arccos, dc.arccosh, dc.arcsin, dc.arcsinh, dc.arctan,
-        dc.arctanh, dc.conjugate, dc.cos, dc.cosh, dc.exp, dc.expm1, dc.invert,
-        dc.log, dc.log10, dc.log1p, dc.negative, dc.reciprocal, dc.rint,
-        dc.sign, dc.sin, dc.sinh, dc.sqrt, dc.square, dc.tan, dc.tanh)
-
-add_checkers(TestLocalArrayUnaryOperations, uops)
 
 
 class TestLocalArrayBinaryOperations(MpiTestCase):
@@ -101,20 +109,34 @@ class TestLocalArrayBinaryOperations(MpiTestCase):
         Check the two- and three-arg ufunc versions as well as the
         method version attached to a LocalArray.
         """
-        x1 = da.ones((16,16), dist=('b',None), comm=self.comm)
-        x2 = da.ones((16,16), dist=('b',None), comm=self.comm)
-        y = da.ones((16,16), dist=('b',None), comm=self.comm)
-        result0 = op(x1, x2)  # standard form
-        op(x1, x2, y=y) # three-arg form
+        x1 = da.ones((16, 16), dist=('b', None), comm=self.comm)
+        x2 = da.ones((16, 16), dist=('b', None), comm=self.comm)
+        y = da.ones((16, 16), dist=('b', None), comm=self.comm)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=RuntimeWarning)
+            result0 = op(x1, x2, casting='unsafe')  # standard form
+            op(x1, x2, y=y, casting='unsafe')  # three-arg form
         assert_array_equal(result0.local_array, y.local_array)
 
 
-bops = (dc.add, dc.arctan2, dc.bitwise_and, dc.bitwise_or, dc.bitwise_xor,
-        dc.divide, dc.floor_divide, dc.fmod, dc.hypot, dc.left_shift, dc.mod,
-        dc.multiply, dc.power, dc.remainder, dc.right_shift, dc.subtract,
-        dc.true_divide)
+uops = (dc.absolute, dc.arccos, dc.arccosh, dc.arcsin, dc.arcsinh, dc.arctan,
+        dc.arctanh, dc.conjugate, dc.cos, dc.cosh, dc.exp, dc.expm1, dc.log,
+        dc.log10, dc.log1p, dc.negative, dc.reciprocal, dc.rint, dc.sign,
+        dc.sin, dc.sinh, dc.sqrt, dc.square, dc.tan, dc.tanh, dc.invert,)
 
-add_checkers(TestLocalArrayBinaryOperations, bops)
+bops = (dc.add, dc.arctan2, dc.divide, dc.floor_divide, dc.fmod, dc.hypot,
+        dc.mod, dc.multiply, dc.power, dc.remainder, dc.subtract,
+        dc.true_divide, dc.less, dc.less_equal, dc.equal, dc.not_equal,
+        dc.greater, dc.greater_equal, dc.bitwise_and, dc.bitwise_or,
+        dc.bitwise_xor, dc.left_shift, dc.right_shift,)
+
+
+# These operations don't work with our default data.
+broken_tests = (dc.invert, dc.bitwise_and, dc.bitwise_or, dc.bitwise_xor,
+                dc.left_shift, dc.right_shift,)
+
+add_checkers(TestLocalArrayUnaryOperations, uops, broken_tests)
+add_checkers(TestLocalArrayBinaryOperations, bops, broken_tests)
 
 
 if __name__ == '__main__':
