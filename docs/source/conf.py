1f067c3f3b74b8bded64323b29365b76f5d10b15
Robert David Grant <rgrant@enthought.com>
2014-02-05 18:04:41 -0600

    Use 'sphinxcontrib.napoleon' instead of 'numpydoc'.
    It works with the numpydoc style and seems to work better.  Also, it's supported by readthedocs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,8 +26,8 @@ sys.path.insert(0, os.path.abspath('../../distarray'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.pngmath', 'numpydoc',
-              'sphinx.ext.autosummary']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.pngmath',
+              'sphinx.ext.autosummary', 'sphinxcontrib.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
