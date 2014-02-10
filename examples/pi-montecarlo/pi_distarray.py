29974cf3a8f0c67895d91fe5b431677bf13de326
Kurt Smith <ksmith@enthought.com>
2014-02-10 12:53:01 -0600

    Tweaks to pi_montecarlo example.
    * Tweaks imports to make parallels between distarray and numpy clear.
    * small formatting touchups.
    * Adds simple @timer decorator.

--- examples/pi-montecarlo/pi_distarray.py
+++ examples/pi-montecarlo/pi_distarray.py
@@ -6,18 +6,21 @@ Usage:
 import sys
 from distarray.client import RandomModule, Context
 
+from util import timer
+
 context = Context()
 random = RandomModule(context)
 
-
+@timer
 def calc_pi(n):
+    """Estimate pi using distributed NumPy arrays."""
     x = random.rand((n,))
     y = random.rand((n,))
     r = context.hypot(x, y)
-    pi = 4 * float((r < 1.).sum())/n
-    context.view.client.purge_everything()
-    return pi
+    return 4 * float((r < 1.).sum())/n
 
 if __name__ == '__main__':
     N = int(sys.argv[1])
-    print(calc_pi(N))
+    result, time = calc_pi(N)
+    print('time  : %3.4g\nresult: %.7f' % (time, result))
+    context.view.client.purge_everything()
