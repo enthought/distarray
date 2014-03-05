17aae283b34c5a32aacff49430be87f939011015
Mark Kness <mkness@enthought.com>
2014-03-05 15:09:06 -0600

    Added tests for invalid comm creation.

--- distarray/local/tests/paralleltest_mpiutils.py
+++ distarray/local/tests/paralleltest_mpiutils.py
@@ -3,8 +3,9 @@ import numpy
 from numpy.testing import assert_array_equal
 
 import distarray.local.denselocalarray as dla
+from distarray.error import InvalidCommSizeError, InvalidRankError
+from distarray.mpiutils import MPI, create_comm_of_size, create_comm_with_list
 from distarray.testing import comm_null_passes
-from distarray.mpiutils import MPI, create_comm_with_list
 
 
 class TestCreateCommWithList(unittest.TestCase):
@@ -26,6 +27,36 @@ class TestCreateCommWithList(unittest.TestCase):
         a = dla.zeros((nrows, 20), comm=self.comm)
         expected = numpy.zeros((nrows // size, 20))
         assert_array_equal(a.local_array, expected)
+
+
+class TestCreateInvalidComm(unittest.TestCase):
+    """ Test that invalid MPI comm creation fails as expected. """
+
+    def setUp(self):
+        # Get the maximum number of nodes available.
+        COMM_PRIVATE = MPI.COMM_WORLD.Clone()
+        self.max_size = COMM_PRIVATE.Get_size()
+
+    def test_size_too_big(self):
+        """ Test that a comm of size with too many nodes will fail. """
+        too_many = 2 * self.max_size
+        with self.assertRaises(InvalidCommSizeError):
+            create_comm_of_size(too_many)
+
+    def test_list_too_big(self):
+        """ Test that a comm from list with too many nodes will fail. """
+        too_many = 2 * self.max_size
+        nodes = range(too_many)
+        with self.assertRaises(InvalidCommSizeError):
+            create_comm_with_list(nodes)
+
+    def test_invalid_ranks(self):
+        """ Test that a comm from list with invalid ranks will fail. """
+        max_size = self.max_size
+        # Nodes should be 0..max_size - 1, so create invalid values.
+        nodes = range(10, max_size + 10)
+        with self.assertRaises(InvalidRankError):
+            create_comm_with_list(nodes)
 
 
 if __name__ == '__main__':
