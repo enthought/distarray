42a586915903d3595f1e0260e2f03a6344b1f3f1
Robert David Grant <rgrant@enthought.com>
2014-03-06 14:21:41 -0600

    Reword docstrings, fix a typo.
    Thanks @kwmsmith, @cowlicks.

--- distarray/context.py
+++ distarray/context.py
@@ -154,10 +154,10 @@ class Context(object):
         name : str or list of str
             If a str, this is used as the prefix for the filename used by each
             engine.  Each engine will save a file named
-            ``<name>_<comm_rank>.dnpy``.
+            ``<name>_<rank>.dnpy``.
             If a list of str, each engine will use the name at the index
-            corresponding to its comm_rank.  Having less or more items in this
-            list than processes is an error.
+            corresponding to its rank.  An exception is raised if the length of
+            this list is not the same as the communicator's size.
         da : DistArray
             Array to save to files.
 
@@ -186,13 +186,13 @@ class Context(object):
 
         Parameters
         ----------
-        name : str or list of
+        name : str or list of str
             If a str, this is used as the prefix for the filename used by each
             engine.  Each engine will load a file named
-            ``<name>_<comm_rank>.dnpy``.
+            ``<name>_<rank>.dnpy``.
             If a list of str, each engine will use the name at the index
-            corresponding to its rank.  Having less or more items in this list
-            than processes is an error.
+            corresponding to its rank.  An exception is raised if the length of
+            this list is not the same as the communicator's size.
 
         Returns
         -------
