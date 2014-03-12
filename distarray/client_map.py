8505024dc85bfd948ba22d59bbb646ea9d4485bc
Kurt Smith <ksmith@enthought.com>
2014-03-12 13:22:13 -0500

    Adds encoding and copyright boilerplate to client_map.py

--- distarray/client_map.py
+++ distarray/client_map.py
@@ -1,3 +1,9 @@
+# encoding: utf-8
+#----------------------------------------------------------------------------
+#  Copyright (C) 2008-2014, IPython Development Team and Enthought, Inc.
+#  Distributed under the terms of the BSD License.  See COPYING.rst.
+#----------------------------------------------------------------------------
+
 from itertools import product
 from distarray.local.base import _start_stop_block
 
