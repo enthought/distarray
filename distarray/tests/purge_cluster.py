a0d78efd944b20a66ebb773c2ff224a42cb9dd93
Mark Kness <mkness@enthought.com>
2014-03-14 15:15:32 -0500

    Added standard file header.

--- distarray/tests/purge_cluster.py
+++ distarray/tests/purge_cluster.py
@@ -1,3 +1,9 @@
+# encoding: utf-8
+#----------------------------------------------------------------------------
+#  Copyright (C) 2008-2014, IPython Development Team and Enthought, Inc.
+#  Distributed under the terms of the BSD License.  See COPYING.rst.
+#----------------------------------------------------------------------------
+
 """ Simple utility to clean out existing namespaces on engines. """
 
 from __future__ import print_function
