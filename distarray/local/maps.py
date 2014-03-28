c8aba993d3dc523a11cf3198ccc1f96ca8ebafd3
Kurt Smith <ksmith@enthought.com>
2014-03-28 11:04:46 -0500

    Renames IndexMap.__init__ in prep for refactoring.

--- distarray/local/maps.py
+++ distarray/local/maps.py
@@ -75,7 +75,10 @@ class IndexMap(object):
         Given a global index as a key, return the corresponding local index.
     """
 
-    def __init__(self, global_indices):
+    # def __init__(self, global_indices):
+        # pass
+
+    def _internal__init__(self, global_indices):
         """Make an IndexMap from a local_index and global_index.
 
         Parameters
@@ -96,4 +99,174 @@ class IndexMap(object):
     def from_dimdict(cls, dimdict):
         """Make an IndexMap from a `dimdict` data structure."""
         global_indices_fn = global_indices_from_dist_type[dimdict['dist_type']]
-        return IndexMap(global_indices_fn(dimdict))
+        self = cls.__new__(cls)
+        self._internal__init__(global_indices_fn(dimdict))
+        return self
+
+
+class BlockMap(IndexMap):
+
+    dist = 'b'
+
+    def __init__(self, global_size, grid_size, grid_rank, start, stop):
+        self.start = start
+        self.stop = stop
+        self.local_size = stop - start
+        self.global_size = global_size
+        self.grid_size = grid_size
+        self.grid_rank = grid_rank
+
+    def local_from_global(self, gidx):
+        if gidx < self.start or gidx >= self.stop:
+            raise IndexError()
+        return gidx - self.start
+
+    def global_from_local(self, lidx):
+        if lidx >= self.local_size:
+            raise IndexError()
+        return lidx + self.start
+
+    def global_slice(self):
+        return slice(self.start, self.stop)
+
+    @property
+    def dim_dict(self):
+        return {'dist_type': self.dist,
+                'size': self.global_size,
+                'proc_grid_rank': self.grid_rank,
+                'proc_grid_size': self.grid_size,
+                'start': self.start,
+                'stop': self.stop,
+                }
+
+
+class CyclicMap(IndexMap):
+
+    dist = 'c'
+
+    def __init__(self, global_size, grid_size, grid_rank, start):
+        if start != grid_rank:
+            raise ValueError()
+        if start >= grid_size:
+            msg = "start (%d) is greater or equal to grid_size (%d)"
+            raise ValueError(msg % (start, grid_size))
+        self.start = start
+        self.grid_size = grid_size
+        self.grid_rank = grid_rank
+
+        self.local_size = (global_size - 1 - grid_rank) // grid_size + 1
+        self.global_size = global_size
+
+    def local_from_global(self, gidx):
+        if (gidx - self.start) % self.grid_size:
+            raise IndexError()
+        return (gidx - self.start) // self.grid_size
+
+    def global_from_local(self, lidx):
+        if lidx >= self.local_size:
+            raise IndexError()
+        return (lidx * self.grid_size) + self.start
+
+    def global_slice(self):
+        return slice(self.start, self.global_size, self.grid_size)
+
+    @property
+    def dim_dict(self):
+        return {'dist_type': self.dist,
+                'size': self.global_size,
+                'proc_grid_rank': self.grid_rank,
+                'proc_grid_size': self.grid_size,
+                'start': self.start,
+                }
+
+
+class BlockCyclicMap(IndexMap):
+
+    dist = 'c'
+    
+    def __init__(self, global_size, grid_size, grid_rank, start, block_size):
+        if start % block_size:
+            raise ValueError()
+        self.start_block = start // block_size
+        self.block_size = block_size
+        global_nblocks = global_size // block_size
+        if global_nblocks * block_size != global_size:
+            raise ValueError()
+        self.grid_size = grid_size
+
+        local_nblocks = (global_nblocks - 1 - grid_rank) // grid_rank + 1
+        self.local_size = local_nblocks * block_size
+        self.global_size = global_size
+
+        # if global_nblocks == grid_size * (global_nblocks // grid_size):
+            # local_nblocks = global_nblocks // grid_size
+        # elif grid_rank == grid_size - 1:
+            # local_nblocks = global_nblocks % grid_size
+        # else:
+            # local_nblocks = global_nblocks // grid_size + 1
+        # self.local_size = local_nblocks * block_size
+
+    def local_from_global(self, gidx):
+        global_block, offset = divmod(gidx, self.block_size)
+        if (global_block - self.start_block) % self.grid_size:
+            raise IndexError()
+        return self.block_size * ((global_block - self.start_block) // self.grid_size) + offset
+
+    def global_from_local(self, lidx):
+        if lidx >= self.local_size:
+            raise IndexError()
+        local_block, offset = divmod(lidx, self.block_size)
+        global_block = (local_block * self.grid_size) + self.start_block
+        return global_block * self.block_size + offset
+
+    def global_slice(self):
+        raise NotImplementedError()
+        # TODO: FIXME: Not really a slice, but a fancy index, since can't
+        # represent blockcyclic as a slice!
+        # idxs = np.empty(self.size, dtype=np.int)
+        # for offset in range(self.block_size):
+
+    @property
+    def dim_dict(self):
+        return {'dist_type': self.dist,
+                'size': self.global_size,
+                'proc_grid_rank': self.grid_rank,
+                'proc_grid_size': self.grid_size,
+                'start': self.start,
+                'block_size': self.block_size,
+                }
+
+
+
+class UnstructuredMap(IndexMap):
+
+    dist = 'u'
+
+    def __init__(self, global_size, grid_size, grid_rank, indices):
+        self.global_size = global_size
+        self.grid_size = grid_size
+        self.grid_rank = grid_rank
+        self.indices = list(indices)
+        self.local_size = len(self.indices)
+
+    def local_from_global(self, gidx):
+        try:
+            lidx = self.indices.index(gidx)
+        except ValueError:
+            raise IndexError()
+        return lidx
+
+    def global_from_local(self, lidx):
+        return self.indices[lidx]
+
+    def global_slice(self):
+        raise NotImplementedError()
+
+    @property
+    def dim_dict(self):
+        return {'dist_type': self.dist,
+                'size': self.global_size,
+                'proc_grid_rank': self.grid_rank,
+                'proc_grid_size': self.grid_size,
+                'indices': self.indices,
+                }
