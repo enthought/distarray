01e1118f05da2646342fbdce9897cafa3ba869ed
Kurt Smith <ksmith@enthought.com>
2014-03-28 13:49:17 -0500

    Replaces IndexMap with class heirarchy.

--- distarray/local/maps.py
+++ distarray/local/maps.py
@@ -10,65 +10,13 @@ from distarray.externals.six.moves import range, zip
 from math import ceil
 
 
-def not_distributed(dd):
-    """Return the global indicies owned by this undistributed process.
-
-    Requires the 'size' key.
-    """
-    return range(dd['size'])
-
-
-def block(dd):
-    """Return the global indices owned by this block-distributed process.
-
-    Requires 'start' and 'stop' keys.
-    """
-    return range(dd['start'], dd['stop'])
-
-
-def cyclic(dd):
-    """Return the global indices owned by this (block-)cyclically-distributed
-    process.
-
-    Requires 'start', 'size', 'proc_grid_size', and (optionally) 'block_size'
-    keys.  If 'block_size' key does not exist, it is set to 1.
-    """
-    dd.setdefault('block_size', 1)
-    nblocks = int(ceil(dd['size'] / dd['block_size']))
-    block_indices = range(0, nblocks, dd['proc_grid_size'])
-
-    global_indices = []
-    for block_index in block_indices:
-        block_start = block_index * dd['block_size'] + dd['start']
-        block_stop = block_start + dd['block_size']
-        block = range(block_start, min(block_stop, dd['size']))
-        global_indices.extend(block)
-
-    return global_indices
-
-
-def unstructured(dd):
-    """Return the arbitrary global indices owned by this  process.
-
-    Requires the 'indices' key.
-    """
-    return dd['indices']
-
-
-global_indices_from_dist_type = {
-    'n': not_distributed,
-    'b': block,
-    'c': cyclic,
-    'u': unstructured,
-}
-
 class MDMap(object):
     
     @classmethod
     def from_dim_data(cls, dim_data):
         self = cls.__new__(cls)
-        self.maps = tuple(IndexMap.from_dimdict(dimdict)
-                          for dimdict in dim_data)
+        self.maps = tuple(_map_from_dim_dict(dimdict)
+                            for dimdict in dim_data)
         self.ndim = len(self.maps)
         return self
 
@@ -91,45 +39,35 @@ class MDMap(object):
         return len(self.maps)
 
 
-class IndexMap(object):
+def _map_from_dim_dict(dd):
 
-    """Provide global->local and local->global index mappings.
+    # Extract parameters from the dimension dictionary.
+    dist_type = dd['dist_type']
+    size = dd['size']
+    start = dd.get('start', None)
+    stop = dd.get('stop', None)
+    grid_rank = dd.get('proc_grid_rank', None)
+    grid_size = dd.get('proc_grid_size', None)
+    block_size = dd.get('block_size', 1)
+    indices = dd.get('indices', None)
 
-    Attributes
-    ----------
-    global_index : list of int or range object
-        Given a local index as a key, return the corresponding global index.
-    local_index : dict of int -> int
-        Given a global index as a key, return the corresponding local index.
-    """
+    if dist_type == 'n':
+        return BlockMap(global_size=size, grid_size=grid_size, grid_rank=grid_rank, start=0, stop=size)
+    if dist_type == 'b':
+        return BlockMap(global_size=size, grid_size=grid_size, grid_rank=grid_rank, start=start, stop=stop)
+    if dist_type == 'c' and block_size == 1:
+        return CyclicMap(global_size=size, grid_size=grid_size, grid_rank=grid_rank, start=start)
+    if dist_type == 'c' and block_size > 1:
+        return BlockCyclicMap(global_size=size, grid_size=grid_size, grid_rank=grid_rank, start=start, block_size=block_size)
+    if dist_type == 'u':
+        return UnstructuredMap(global_size=size, grid_size=grid_size, grid_rank=grid_rank, indices=indices)
+    raise ValueError("Unsupported dist_type of %r" % dist_type)
 
-    def _internal__init__(self, global_indices):
-        """Make an IndexMap from a local_index and global_index.
-
-        Parameters
-        ----------
-        global_indices: list of int or range object
-            Each position contains the corresponding global index for a
-            local index (position).
-        """
-        self.global_index = global_indices
-        local_indices = range(len(global_indices))
-        self.local_index = dict(zip(global_indices, local_indices))
-
-    @property
-    def size(self):
-        return len(self.global_index)
-
-    @classmethod
-    def from_dimdict(cls, dimdict):
-        """Make an IndexMap from a `dimdict` data structure."""
-        global_indices_fn = global_indices_from_dist_type[dimdict['dist_type']]
-        self = cls.__new__(cls)
-        self._internal__init__(global_indices_fn(dimdict))
-        return self
+class MapBase(object):
+    pass
 
 
-class BlockMap(IndexMap):
+class BlockMap(MapBase):
 
     dist = 'b'
 
@@ -140,6 +78,10 @@ class BlockMap(IndexMap):
         self.global_size = global_size
         self.grid_size = grid_size
         self.grid_rank = grid_rank
+
+        self._global_index = list(range(self.start, self.stop))
+        local_indices = range(len(self._global_index))
+        self._local_index = dict(zip(self._global_index, local_indices))
 
     def local_from_global(self, gidx):
         if gidx < self.start or gidx >= self.stop:
@@ -164,8 +106,20 @@ class BlockMap(IndexMap):
                 'stop': self.stop,
                 }
 
+    @property
+    def global_index(self):
+        return self._global_index
 
-class CyclicMap(IndexMap):
+    @property
+    def local_index(self):
+        return self._local_index
+
+    @property
+    def size(self):
+        return len(self.global_index)
+
+
+class CyclicMap(MapBase):
 
     dist = 'c'
 
@@ -181,6 +135,10 @@ class CyclicMap(IndexMap):
 
         self.local_size = (global_size - 1 - grid_rank) // grid_size + 1
         self.global_size = global_size
+
+        self._global_index = list(range(self.start, self.global_size, self.grid_size))
+        local_indices = range(len(self._global_index))
+        self._local_index = dict(zip(self._global_index, local_indices))
 
     def local_from_global(self, gidx):
         if (gidx - self.start) % self.grid_size:
@@ -204,8 +162,21 @@ class CyclicMap(IndexMap):
                 'start': self.start,
                 }
 
+    @property
+    def global_index(self):
+        return self._global_index
 
-class BlockCyclicMap(IndexMap):
+    @property
+    def local_index(self):
+        return self._local_index
+
+    @property
+    def size(self):
+        return len(self.global_index)
+
+
+
+class BlockCyclicMap(MapBase):
 
     dist = 'c'
     
@@ -263,7 +234,7 @@ class BlockCyclicMap(IndexMap):
 
 
 
-class UnstructuredMap(IndexMap):
+class UnstructuredMap(MapBase):
 
     dist = 'u'
 
@@ -273,6 +244,8 @@ class UnstructuredMap(IndexMap):
         self.grid_rank = grid_rank
         self.indices = list(indices)
         self.local_size = len(self.indices)
+        local_indices = range(len(self.indices))
+        self._local_index = dict(zip(self.indices, local_indices))
 
     def local_from_global(self, gidx):
         try:
@@ -295,3 +268,106 @@ class UnstructuredMap(IndexMap):
                 'proc_grid_size': self.grid_size,
                 'indices': self.indices,
                 }
+
+    @property
+    def global_index(self):
+        return self.indices
+
+    @property
+    def local_index(self):
+        return self._local_index
+
+    @property
+    def size(self):
+        return len(self.global_index)
+
+
+class IndexMap(object):
+
+    """Provide global->local and local->global index mappings.
+
+    Attributes
+    ----------
+    global_index : list of int or range object
+        Given a local index as a key, return the corresponding global index.
+    local_index : dict of int -> int
+        Given a global index as a key, return the corresponding local index.
+    """
+
+    def _internal__init__(self, global_indices):
+        """Make an IndexMap from a local_index and global_index.
+
+        Parameters
+        ----------
+        global_indices: list of int or range object
+            Each position contains the corresponding global index for a
+            local index (position).
+        """
+        self.global_index = global_indices
+        local_indices = range(len(global_indices))
+        self.local_index = dict(zip(global_indices, local_indices))
+
+    @property
+    def size(self):
+        return len(self.global_index)
+
+    @classmethod
+    def from_dimdict(cls, dimdict):
+        """Make an IndexMap from a `dimdict` data structure."""
+        global_indices_fn = global_indices_from_dist_type[dimdict['dist_type']]
+        self = cls.__new__(cls)
+        self._internal__init__(global_indices_fn(dimdict))
+        return self
+
+
+def not_distributed(dd):
+    """Return the global indicies owned by this undistributed process.
+
+    Requires the 'size' key.
+    """
+    return range(dd['size'])
+
+
+def block(dd):
+    """Return the global indices owned by this block-distributed process.
+
+    Requires 'start' and 'stop' keys.
+    """
+    return range(dd['start'], dd['stop'])
+
+
+def cyclic(dd):
+    """Return the global indices owned by this (block-)cyclically-distributed
+    process.
+
+    Requires 'start', 'size', 'proc_grid_size', and (optionally) 'block_size'
+    keys.  If 'block_size' key does not exist, it is set to 1.
+    """
+    dd.setdefault('block_size', 1)
+    nblocks = int(ceil(dd['size'] / dd['block_size']))
+    block_indices = range(0, nblocks, dd['proc_grid_size'])
+
+    global_indices = []
+    for block_index in block_indices:
+        block_start = block_index * dd['block_size'] + dd['start']
+        block_stop = block_start + dd['block_size']
+        block = range(block_start, min(block_stop, dd['size']))
+        global_indices.extend(block)
+
+    return global_indices
+
+
+def unstructured(dd):
+    """Return the arbitrary global indices owned by this  process.
+
+    Requires the 'indices' key.
+    """
+    return dd['indices']
+
+
+global_indices_from_dist_type = {
+    'n': not_distributed,
+    'b': block,
+    'c': cyclic,
+    'u': unstructured,
+}
