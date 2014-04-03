fedc41c0193bc5704feb759d69e8e5654446855c
Blake Griffith <blake.a.griffith@gmail.com>
2014-04-03 13:30:42 -0500

    Change install script to reflect new git-hooks/ location.

--- utils/git-hooks/install-hooks.sh
+++ utils/git-hooks/install-hooks.sh
@@ -3,6 +3,6 @@
 DOTGIT=`git rev-parse --git-dir`
 TOPLEVEL=`git rev-parse --show-toplevel`
 TO=${DOTGIT}/hooks
-FROM=${TOPLEVEL}/git-hooks
+FROM=${TOPLEVEL}/utils/git-hooks
 
 ln -s ${FROM}/pre-commit ${TO}/pre-commit
