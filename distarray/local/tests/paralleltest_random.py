c62f246eb756a5662059d9fa0dd17461fab0eaa1
Mark Kness <mkness@enthought.com>
2014-04-08 10:36:54 -0500

    Added coverage for random label_state.

--- distarray/local/tests/paralleltest_random.py
+++ distarray/local/tests/paralleltest_random.py
@@ -5,6 +5,7 @@
 # ---------------------------------------------------------------------------
 
 import unittest
+import numpy as np
 
 from distarray.local import random as local_random
 from distarray.testing import MpiTestCase
@@ -22,13 +23,24 @@ class TestBasic(MpiTestCase):
         self.assertTrue(la.comm_rank in range(4))
         self.assertEqual(la.comm.Get_topo(),
                          (list(la.grid_shape),
-                          [0,0],list(la.cart_coords)))
+                          [0, 0], list(la.cart_coords)))
         self.assertEqual(len(la.maps), 2)
         self.assertEqual(la.global_shape, (16, 16))
         self.assertEqual(la.grid_shape, (4, 1))
         self.assertEqual(la.local_shape, (4, 16))
         self.assertEqual(la.local_array.shape, la.local_shape)
         self.assertEqual(la.local_array.dtype, la.dtype)
+
+    def test_label_state(self):
+        """ Test we can label the local random generator with the rank. """
+        # This test is mainly intended for coverage, as the client-side
+        # test of the behavior does not label the routine as covered.
+        s0, orig_array, s2, s3, s4 = np.random.get_state()
+        local_random.label_state(self.comm)
+        s0, rank_array, s2, s3, s4 = np.random.get_state()
+        # State should have changed from labeling.
+        state_equal = np.all(orig_array == rank_array)
+        self.assertFalse(state_equal)
 
     def test_beta(self):
         la = local_random.beta(2, 5, size=(16, 16), grid_shape=(4, 1), comm=self.comm)
