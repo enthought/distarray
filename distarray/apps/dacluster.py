f00bffb9215e76c6c4618229b9ac4ad55240d5c4
Blake Griffith <blake.a.griffith@gmail.com>
2014-04-11 11:19:05 -0500

    Add help text. Move module level code into main(). Add dump.

--- distarray/apps/dacluster.py
+++ distarray/apps/dacluster.py
@@ -24,36 +24,80 @@ class ArgumentParser(argparse.ArgumentParser):
         ipcluster_tools.run_ipcluster(sys.argv[1:])
 
 
-description = """
-Start, stop and manage a IPython.parallel cluster. `dacluster` can take
-all the commands IPython's `ipcluster` can, and a few extras that are
-distarray specific.
-"""
-parser = ArgumentParser(description=description)
-
-# Print help if no command line args are supplied
-if len(sys.argv) == 1:
-    parser.print_help()
-    sys.exit(1)
-
-
-subparsers = parser.add_subparsers(help='subparsers')
-
-# subparses for all our commands
-parser_start = subparsers.add_parser('start')
-parser_stop = subparsers.add_parser('stop')
-parser_restart = subparsers.add_parser('restart')
-parser_clear = subparsers.add_parser('clear')
-parser_purge = subparsers.add_parser('purge')
-
-# set the functions each command should use
-parser_start.set_defaults(func=ipcluster_tools.start)
-parser_stop.set_defaults(func=ipcluster_tools.stop)
-parser_restart.set_defaults(func=ipcluster_tools.restart)
-parser_clear.set_defaults(func=ipcluster_tools.clear)
-parser_purge.set_defaults(func=purge_cluster.purge)
-
-
 def main():
+    main_description = """
+    Start, stop and manage a IPython.parallel cluster. `dacluster` can take
+    all the commands IPython's `ipcluster` can, and a few extras that are
+    distarray specific.
+    """
+    parser = ArgumentParser(description=main_description)
+
+    # Print help if no command line args are supplied
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+
+    subparsers = parser.add_subparsers()
+
+    start_description = """
+    Start a new IPython.parallel cluster.
+    """
+
+    stop_description = """
+    Stop a IPython.parallel cluster.
+    """
+
+    restart_description = """
+    Restart a IPython.parallel cluster.
+    """
+
+    clear_description = """
+    Clear the namespace and imports on the cluster. This should be the
+    same as restarting the engines, but faster.
+    """
+
+    purge_description = """
+    Clear all the DistArray objects from the engines. This has a few
+    leaks.
+    """
+
+    dump_description = """
+    Print out key names that exist on the engines.
+    """
+
+    # subparses for all our commands
+    parser_start = subparsers.add_parser('start',
+                                         description=start_description)
+    parser_stop = subparsers.add_parser('stop', description=stop_description)
+    parser_restart = subparsers.add_parser('restart',
+                                           description=restart_description)
+    parser_clear = subparsers.add_parser('clear',
+                                         description=clear_description)
+    parser_purge = subparsers.add_parser('purge',
+                                         description=purge_description)
+    parser_dump = subparsers.add_parser('dump', description=dump_description)
+
+    engine_help = """
+    Number of engines to start.
+    """
+
+    # Add some optional arguments for `start` and `restart`
+    parser_start.add_argument('-n', '--n', type=int, nargs='?', default=4,
+                              help=engine_help)
+    parser_restart.add_argument('-n', '--n', type=int, nargs='?', default=4,
+                                help=engine_help)
+
+    # set the functions each command should use
+    parser_start.set_defaults(func=ipcluster_tools.start)
+    parser_stop.set_defaults(func=ipcluster_tools.stop)
+    parser_restart.set_defaults(func=ipcluster_tools.restart)
+    parser_clear.set_defaults(func=ipcluster_tools.clear)
+    parser_purge.set_defaults(func=purge_cluster.purge)
+    parser_dump.set_defaults(func=purge_cluster.dump)
+
+    # run it
     args = parser.parse_args()
-    args.func()
+    args.func(**vars(args))
+
+if __name__ == '__main__':
+    main()
