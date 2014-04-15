464598a4308a16d9b3c52657b3f17f4880d86130
Blake Griffith <blake.a.griffith@gmail.com>
2014-04-15 12:32:01 -0500

    Move ipcluster_tools.py functions into dacluster.py

--- distarray/apps/dacluster.py
+++ distarray/apps/dacluster.py
@@ -10,10 +10,116 @@ all the commands IPython's `ipcluster` can, and a few extras that are
 distarray specific.
 """
 
+from __future__ import print_function
+
 import argparse
 import sys
+from time import sleep
+from subprocess import Popen, PIPE
 
-from . import ipcluster_tools
+from distarray.externals import six
+from distarray.context import Context
+
+
+if six.PY2:
+    ipcluster_cmd = 'ipcluster'
+elif six.PY3:
+    ipcluster_cmd = 'ipcluster3'
+else:
+    raise NotImplementedError("Not run with Python 2 *or* 3?")
+
+
+def start(n=4, engines=None, **kwargs):
+    """Convenient way to start an ipcluster for testing.
+
+    Doesn't exit until the ipcluster prints a success message.
+    """
+    if engines is None:
+        engines = "--engines=MPIEngineSetLauncher"
+
+    cluster = Popen([ipcluster_cmd, 'start', '-n', str(n), engines],
+                    stdout=PIPE, stderr=PIPE)
+
+    started = "Engines appear to have started successfully"
+    running = "CRITICAL | Cluster is already running with"
+    while True:
+        line = cluster.stderr.readline().decode()
+        if not line:
+            break
+        print(line, end='')
+        if (started in line):
+            break
+        elif (running in line):
+            raise RuntimeError("ipcluster is already running.")
+
+
+def stop(**kwargs):
+    """Convenient way to stop an ipcluster."""
+    stopping = Popen([ipcluster_cmd, 'stop'], stdout=PIPE, stderr=PIPE)
+
+    stopped = "Stopping cluster"
+    not_running = ("CRITICAL | Could not read pid file, cluster "
+                   "is probably not running.")
+    while True:
+        line = stopping.stderr.readline().decode()
+        if not line:
+            break
+        print(line, end='')
+        if (stopped in line) or (not_running in line):
+            break
+
+
+def restart(n=4, engines=None, **kwargs):
+    """Convenient way to restart an ipcluster."""
+    stop()
+
+    started = False
+    while not started:
+        sleep(2)
+        try:
+            start(n=n, engines=engines)
+        except RuntimeError:
+            pass
+        else:
+            started = True
+
+_RESET_ENGINE_DISTARRAY = '''
+from sys import modules
+orig_mods = set(modules)
+for m in modules.copy():
+    if m.startswith('distarray'):
+        del modules[m]
+deleted_mods = sorted(orig_mods - set(modules))
+'''
+
+
+def clear(**kwargs):
+    from IPython.parallel import Client
+    c = Client()
+    dv = c[:]
+    dv.execute(_RESET_ENGINE_DISTARRAY, block=True)
+    mods = dv['deleted_mods']
+    print("The following modules were removed from the engines' namespaces:")
+    for mod in mods[0]:
+        print('    ' + mod)
+    dv.clear()
+
+
+def dump(**kwargs):
+    """ Print out key names that exist on the engines. """
+    context = Context()
+    keylist = context.dump_keys(all_other_contexts=True)
+    num_keys = len(keylist)
+    print('*** %d ENGINE KEYS ***' % (num_keys))
+    for key, targets in keylist:
+        print('%s : %r' % (key, targets))
+
+
+def purge(**kwargs):
+    """ Remove keys from the engine namespaces. """
+    print('Purging keys from engines...')
+    context = Context()
+    context.cleanup(all_other_contexts=True)
 
 
 def main():
@@ -81,12 +187,12 @@ def main():
                                 help=engine_help)
 
     # set the functions each command should use
-    parser_start.set_defaults(func=ipcluster_tools.start)
-    parser_stop.set_defaults(func=ipcluster_tools.stop)
-    parser_restart.set_defaults(func=ipcluster_tools.restart)
-    parser_clear.set_defaults(func=ipcluster_tools.clear)
-    parser_purge.set_defaults(func=ipcluster_tools.purge)
-    parser_dump.set_defaults(func=ipcluster_tools.dump)
+    parser_start.set_defaults(func=start)
+    parser_stop.set_defaults(func=stop)
+    parser_restart.set_defaults(func=restart)
+    parser_clear.set_defaults(func=clear)
+    parser_purge.set_defaults(func=purge)
+    parser_dump.set_defaults(func=dump)
 
     # run it
     args = parser.parse_args()
