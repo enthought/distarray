cbb6699222b0fdf10fab3e0eb8a6d039ab714f73
Kurt Smith <ksmith@enthought.com>
2014-04-17 17:16:12 -0500

    Updates version to 0.3-dev.
    Helps indicate that we're now working on 0.3 pre-release.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ install_requires = [
 
 metadata = {
     'name': 'distarray',
-    'version': '0.2.0',
+    'version': '0.3.0-dev',
     'description': 'Distributed Memory Arrays for Python',
     'keywords': 'parallel mpi distributed array',
     'license': 'New BSD',
