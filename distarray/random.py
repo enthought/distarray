7272d21b47e81659f2c58b19eb5a8c13b2ea5d15
Robert David Grant <rgrant@enthought.com>
2014-04-30 19:25:43 -0500

    Modify Random.randn to use new from_localarrays

--- distarray/random.py
+++ distarray/random.py
@@ -241,14 +241,17 @@ class Random(object):
         if dist is None:
             dist = {0: 'b'}
         da_key = self.context._generate_key()
-        size_key, dist_key, grid_shape_key = \
-            self.context._key_and_push(size, dist, grid_shape)
-        comm_key = self.context._comm_key
+
+        distribution = Distribution.from_shape(context=self.context,
+                                               shape=size,
+                                               dist=dist,
+                                               grid_shape=grid_shape)
+        ddpr = distribution.get_dim_data_per_rank()
+        ddpr_name = self.context._key_and_push(ddpr)[0]
+        comm_name = self.context._comm_key
         self.context._execute(
             '{da_key} = distarray.local.random.randn('
-            'distribution=distarray.local.maps.Distribution.from_shape('
-            'shape={size_key}, dist={dist_key},'
-            'grid_shape={grid_shape_key}, comm={comm_key}'
-            '))'.format(**locals())
-        )
-        return DistArray.from_localarrays(da_key, self.context)
+            'distribution=distarray.local.maps.Distribution('
+            'dim_data={ddpr_name}[{comm_name}.Get_rank()], '
+            'comm={comm_name}))'.format(**locals()))
+        return DistArray.from_localarrays(da_key, distribution=distribution)
