9e9caf5b884727dea40940d04f6581cbdabebaab
Blake Griffith <blake.a.griffith@gmail.com>
2014-05-27 16:46:23 -0500

    import random module into distarray.dist

--- distarray/dist/__init__.py
+++ distarray/dist/__init__.py
@@ -9,4 +9,5 @@ from __future__ import absolute_import
 from distarray.dist.distarray import DistArray
 from distarray.dist.context import Context
 from distarray.dist.maps import Distribution
+from distarray.dist import random
 from distarray.dist.functions import *
