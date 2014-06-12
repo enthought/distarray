085cc31af0655f92124f9fd7acca11294aa51ed5
Robert David Grant <rgrant@enthought.com>
2014-06-12 17:49:57 -0500

    Improve failing test.

--- distarray/dist/tests/test_distarray.py
+++ distarray/dist/tests/test_distarray.py
@@ -313,14 +313,11 @@ class TestSetItemSlicing(ContextTestCase):
         with self.assertRaises(ValueError):
             arr[slc] = new_data
 
-    def test_resulting_slice(self):
+    def test_set_DistArray_slice(self):
         dist = Distribution.from_shape(self.context, (10, 20))
         da = self.context.ones(dist)
-        db = da[:5, :10]
-        arr = db.tondarray()
-        db[...] = arr * 2
-        assert_array_equal(db.toarray(), numpy.ones(db.shape) * 2)
-        db[...] = db * 2
+        db = self.context.zeros(dist)
+        da[...] = db
 
 
 class TestDistArrayCreationFromGlobalDimData(ContextTestCase):
