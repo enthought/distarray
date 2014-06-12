ae3dd407b25dd775fe39d804694eef23eb7d2b33
Blake Griffith <blake.a.griffith@gmail.com>
2014-06-12 17:23:56 -0500

    Add regression test for gh-issue-435

--- distarray/dist/tests/test_distarray.py
+++ distarray/dist/tests/test_distarray.py
@@ -595,6 +595,11 @@ class TestReduceMethods(ContextTestCase):
             assert_allclose(darr_sum.tondarray(), arr_sum)
         assert_allclose(darr.sum().tondarray(), arr.sum())
 
+    def test_gh_435_regression_with_var(self):
+        dist = Distribution.from_shape(self.context, shape=(14,), dist=('b'))
+        darr = self.context.ones(dist)
+        darr.var()
+
 
 class TestFromLocalArrays(ContextTestCase):
 
