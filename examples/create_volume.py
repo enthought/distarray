eb15f8baac00ef98d8f4b2d1aa203bc51655bf19
Mark Kness <mkness@enthought.com>
2014-06-25 12:08:00 -0500

    Much more efficient volume creation.

--- examples/create_volume.py
+++ examples/create_volume.py
@@ -12,13 +12,14 @@ import h5py
 PHYSICAL_SIZE = (10.0, 10.0, 20.0)
 
 # Array size of volume.
+#ARRAY_SIZE = (2, 2, 10)
 ARRAY_SIZE = (4, 4, 25)
 #ARRAY_SIZE = (512, 512, 1024)
-#ARRAY_SIZE = (2, 2, 10)
+#ARRAY_SIZE = (512, 512, 2048)
 
 def g(z, z0, A=100.0, C=2.5, mu=0.1):
     '''Gaussian.  Put a peak at depth z=z0.'''
-    g = A * exp(-mu * (z - z0)**2) + C
+    g = A * numpy.exp(-mu * (z - z0)**2) + C
     return g
 
 def p(x, y):
@@ -54,37 +55,42 @@ def create_horizon():
 
 def create_volume():
     print 'Creating volume array...'
+    # No randomness right now...
     #vol = numpy.random.randn(SIZE[0], SIZE[1], SIZE[2])
     vol = numpy.zeros(ARRAY_SIZE, dtype=numpy.float32)
     print 'Creating horizon...'
     horizon = create_horizon()
     print 'Filling array...'
+    z = numpy.empty((ARRAY_SIZE[2],))
+    for k in xrange(ARRAY_SIZE[2]):
+        z[k] = get_z(k)
     for i in xrange(ARRAY_SIZE[0]):
-        print i, ARRAY_SIZE[0]
+        print 'Index', i, 'of', ARRAY_SIZE[0]
         for j in xrange(ARRAY_SIZE[1]):
             z0 = horizon[i, j]
-            for k in xrange(ARRAY_SIZE[2]):
-                z = get_z(k)
-                vol[i, j, k] = g(z, z0)
+            vol[i, j, :] = g(z, z0)
     print 'Done.'
     return vol
 
-def create_file(volume):
+def create_file(volume, filename, key):
     '''Create an HDF5 file with the seismic volume.'''
-    f = h5py.File("seismic.hdf5", "w")
-    dataset = f.create_dataset("seismic", ARRAY_SIZE, dtype='f')
+    f = h5py.File(filename, 'w')
+    dataset = f.create_dataset(key, ARRAY_SIZE, dtype='f')
+    dataset[...] = volume
     print "Dataset dataspace is", dataset.shape
     print "Dataset Numpy datatype is", dataset.dtype
     print "Dataset name is", dataset.name
     print "Dataset is a member of the group", dataset.parent
     print "Dataset was created in the file", dataset.file
-    dataset[...] = volume
     f.close()
-    
+
 def main():
+    filename = 'seismic.hdf5'
+    key = 'seismic'
     vol = create_volume()
-    print vol
-    create_file(vol)
+    if False:
+        print vol
+    create_file(vol, filename=filename, key=key)
 
 if __name__ == '__main__':
     main()
