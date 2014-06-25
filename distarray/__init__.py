9235bbbc2cb49f9296ac444700c35ffa2b92891e
Blake Griffith <blake.a.griffith@gmail.com>
2014-06-25 16:40:45 -0500

    Add global vars to distarray/__init__.py for managing MPI comms

--- distarray/__init__.py
+++ distarray/__init__.py
@@ -5,4 +5,8 @@
 # ---------------------------------------------------------------------------
 
 from distarray.__version__ import __version__
-DISTARRAY_BASE_NAME = "__distarray__"
+DISTARRAY_BASE_NAME = '__distarray__'
+# intracomm consisting of all the engines, used in MPI mode.
+_BASE_COMM = None
+# Intercomm left group is client, right group is all engines, used in MPI mode.
+INTERCOMM = None
