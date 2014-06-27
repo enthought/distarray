28729c9f21cafb77c58b420d52aeef4ba35210dd
Robert David Grant <rgrant@enthought.com>
2014-06-27 11:53:01 -0500

    Test setting ndarray slices with distarrays.

--- distarray/dist/tests/test_distarray.py
+++ distarray/dist/tests/test_distarray.py
@@ -112,6 +112,20 @@ class TestDistArray(ContextTestCase):
         assert_array_equal(darr.tondarray(), nparr)
         assert_array_equal(numpy.ones((7, 9)), nparr)
 
+    def test_set_numpy_array_full_slice_with_distarray(self):
+        distribution = Distribution.from_shape(self.context, (7, 9))
+        darr = self.context.ones(distribution)
+        nparr = numpy.zeros_like(darr)
+        nparr[...] = darr
+        assert_array_equal(nparr, darr.toarray())
+
+    def test_set_numpy_array_partial_slice_with_distarray(self):
+        distribution = Distribution.from_shape(self.context, (15, 4))
+        darr = self.context.ones(distribution)
+        nparr = numpy.zeros_like(darr)
+        nparr[3, :] = darr[3, :]
+        assert_array_equal(nparr[3, :], darr[3, :].toarray())
+
     def test_global_tolocal_bug(self):
         # gh-issue #154
         distribution = Distribution.from_shape(self.context, (3, 3),
