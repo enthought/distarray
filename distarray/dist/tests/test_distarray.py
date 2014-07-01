8cecc5d7d323226bcf0da393f6d37b68a580fe4f
Kurt Smith <ksmith@enthought.com>
2014-07-01 17:12:33 -0500

    Use assert_array_equal and assert_allclose.

--- distarray/dist/tests/test_distarray.py
+++ distarray/dist/tests/test_distarray.py
@@ -852,15 +852,16 @@ class TestReduceMethods(ContextTestCase):
         darr.var()
 
     def test_boolean_reduction(self):
-        dist = Distribution.from_shape(self.context, shape=(200,), dist=('b'),
-                                       targets=range(4))
-        darr = self.context.ones(dist)
+        nparr = numpy.random.randn(200)
+        darr = self.context.fromarray(nparr)
         mask = darr < 2
-        self.assertEqual(mask.min().tondarray(), mask.tondarray().min())
-        self.assertEqual(mask.max().tondarray(), mask.tondarray().max())
-        self.assertEqual(mask.mean().tondarray(), mask.tondarray().mean())
-        self.assertEqual(mask.var().tondarray(), mask.tondarray().var())
-        self.assertEqual(mask.sum().tondarray(), mask.tondarray().sum())
+        np_mask = mask.tondarray()
+        assert_array_equal(mask.min().tondarray(), np_mask.min())
+        assert_array_equal(mask.max().tondarray(), np_mask.max())
+        assert_array_equal(mask.sum().tondarray(), np_mask.sum())
+        assert_array_equal(mask.mean().tondarray(), np_mask.mean())
+        assert_allclose(mask.var().tondarray(), np_mask.var())
+        assert_allclose(mask.std().tondarray(), np_mask.std())
 
 class TestFromLocalArrays(ContextTestCase):
 
