83c3aa219d2907986e6b6bc56d57cf43d765d0b1
Mark Kness <mkness@enthought.com>
2014-07-03 10:43:38 -0500

    Another trivial change to force commit.

--- examples/julia-set/julia_example.py
+++ examples/julia-set/julia_example.py
@@ -225,7 +225,7 @@ if __name__ == '__main__':
     im_ax = (-1.5, 1.5)
 
     # Size of number that we consider as going off to infinity.
-    # I think that 2 is sufficient to be sure that the point will escape.
+    # I think that 2.0 is sufficient to be sure that the point will escape.
     z_max = 2.0
 
     # Maximum iteration counts. Points in the set will hit this limit,
