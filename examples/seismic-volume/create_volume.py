ce0ecba577a88082b3fbb71999c2dbd641c3fb39
Mark Kness <mkness@enthought.com>
2014-07-07 17:57:59 -0500

    First pass at creating data via distarray.

--- examples/seismic-volume/create_volume.py
+++ examples/seismic-volume/create_volume.py
@@ -79,7 +79,7 @@ def p(x, y, normal=(0.1, -0.2, 1.0), D=10.0):
     return z0
 
 
-def create_horizon(shape, normal=(0.1, -0.2, 1.0), D=10.0):
+def create_horizon(context, shape, normal=(0.1, -0.2, 1.0), D=10.0):
     '''Get the horizon surface where we will place the peak.'''
     if len(shape) != 2:
         raise ValueError('Horizon shape must be 2D.')
@@ -89,20 +89,26 @@ def create_horizon(shape, normal=(0.1, -0.2, 1.0), D=10.0):
         for j in xrange(shape[1]):
             y = PHYSICAL_Y * float(j) / float(shape[1])
             horizon[i, j] = p(x, y, normal=normal, D=D)
-    return horizon
+    # Wrap it as distarray.
+    horizon_dist = ('b', 'b')
+    distribution = Distribution(context, shape, dist=horizon_dist)
+    horizon_da = DistArray(distribution, dtype=float32)
+    # Fill the array.
+    horizon_da[:, :] = horizon[:, :]
+    return horizon, horizon_da
 
 
-def create_horizons(shape):
+def create_horizons(context, shape):
     '''Create some horizons.'''
-    horizon_1 = create_horizon(shape, normal=(0.1, -0.2, 1.0), D=10.0)
+    horizon_1, horizon_1_da = create_horizon(context, shape, normal=(0.1, -0.2, 1.0), D=10.0)
     params_1 = (100.0, 40.0, 0.0, 0.1)
-    horizon_2 = create_horizon(shape, normal=(0.4, 0.1, 1.0), D=15.0)
+    horizon_2, horizon_2_da = create_horizon(context, shape, normal=(0.4, 0.1, 1.0), D=15.0)
     params_2 = (25.0, 15.0, 0.0, 0.7)
-    rtn = [(horizon_1, params_1), (horizon_2, params_2)]
+    rtn = [(horizon_1, horizon_1_da, params_1), (horizon_2, horizon_2_da, params_2)]
     return rtn
 
 
-def create_volume(shape):
+def create_volume(context, shape):
     ''' Create a fake seismic volume. We try to make it look interesting. '''
     if len(shape) != 3:
         raise ValueError('Volume shape must be 3D.')
@@ -111,7 +117,7 @@ def create_volume(shape):
     vol = numpy.zeros(shape, dtype=numpy.float32)
     print('Creating horizons...')
     horizons_shape = (shape[0], shape[1])
-    horizons = create_horizons(horizons_shape)
+    horizons = create_horizons(context, horizons_shape)
     print('Filling array...')
     z = numpy.empty((shape[2],))
     for k in xrange(shape[2]):
@@ -121,7 +127,7 @@ def create_volume(shape):
         sys.stdout.flush()
         for j in xrange(shape[1]):
             for horizon in horizons:
-                horizon_plane, horizon_params = horizon
+                horizon_plane, horizon_plane_da, horizon_params = horizon
                 z0 = horizon_plane[i, j]
                 A, B, C, mu = horizon_params
                 vol[i, j, :] += g(z, z0, A=A, B=B, C=C, mu=mu)
@@ -133,6 +139,88 @@ def create_volume(shape):
     vol[:, :, :] += 2.0 * rnd
     print('Done.')
     return vol
+
+
+def local_add_horizon(la, lh, z, h_params):
+    ''' Filter a local array via 3-point average over z axis. '''
+
+    def g(z, z0, A=100.0, B=40.0, C=2.5, mu=0.1):
+        ''' A hopefully interesting looking function.
+
+        Gaussian, with a step-like function past the gaussian peak.
+        The peak is at depth z=z0.
+        '''
+        from numpy import exp
+        u = mu * (z - z0) ** 2
+        v = mu * (z - z0)
+        # Gaussian.
+        g = A * exp(-u) + C
+        # Smooth step.
+        exp_p = exp(v)
+        exp_m = exp(-v)
+        h = B * (exp_p - exp_m) / (exp_p + exp_m)
+        r = g + h
+        return r
+
+    def add_horizon(a, h, z, h_params):
+        ''' Add the effect of a horizon to the seismic volume data. '''
+        shape = h.shape
+        A, B, C, mu = h_params
+        for i in xrange(shape[0]):
+            for j in xrange(shape[1]):
+                z0 = h[i, j]
+                a[i, j, :] += g(z, z0, A=A, B=B, C=C, mu=mu)
+        return a
+
+    a = la.ndarray
+    h = lh.ndarray
+    add_horizon(a, h, z, h_params)
+    return la
+
+
+def local_add_random(la):
+    ''' Add randomness and a constant to the local array data. '''
+
+    def add_random(a):
+        ''' Add randomness and a constant to the volume. '''
+        # Add constant
+        a[:, :, :] += 2.5
+        # Add randomness.
+        shape = a.shape
+        rnd = numpy.random.randn(*shape)
+        a[:, :, :] += 2.0 * rnd
+
+    a = la.ndarray
+    add_random(a)
+    return la
+
+
+def distributed_create_volume(context, shape):
+    ''' Create a fake seismic volume. We try to make it look interesting. '''
+    if len(shape) != 3:
+        raise ValueError('Volume shape must be 3D.')
+    print('Creating volume array %d x %d x %d...' % (
+        shape[0], shape[1], shape[2]))
+    dist = ('b', 'b', 'n')
+    distribution = Distribution(context, shape, dist=dist)
+    da = context.zeros(distribution, dtype=float32)
+    print('Creating horizons...')
+    horizons_shape = (shape[0], shape[1])
+    horizons = create_horizons(context, horizons_shape)
+    print('Filling array...')
+    z = numpy.empty((shape[2],))
+    for k in xrange(shape[2]):
+        z[k] = PHYSICAL_Z * float(k) / float(shape[2])
+    # Add data for each horizon...
+    for ih, horizon in enumerate(horizons):
+        print('Horizon %d...' % (ih + 1))
+        horizon_np, horizon_da, horizon_params = horizon
+        context.apply(local_add_horizon, (da.key, horizon_da.key, z, horizon_params))
+    # Add global randomness.
+    print('Adding randomness...')
+    context.apply(local_add_random, (da.key,))
+    print('Done.')
+    return da
 
 
 def create_hdf5_file(volume, filename, key):
@@ -200,15 +288,20 @@ def main():
         use_hdf5 = True
     if (use_hdf5 == True) and (use_dnpy == True):
         raise ValueError('Can only specify one of --hdf5 or --dnpy.')
+    # Create context.
+    context = Context()
     # Create the seismic volume and write it.
     t0 = time()
-    vol = create_volume(shape)
+    vol = create_volume(context, shape)
+    da_vol = distributed_create_volume(context, shape)
     if use_hdf5:
         print('Creating hdf5 file...')
-        create_hdf5_file(vol, filename=filename, key=key)
+        #create_hdf5_file(vol, filename=filename, key=key)
+        create_hdf5_file(da_vol.tondarray(), filename=filename, key=key)
     elif use_dnpy:
         print('Creating dnpy files...')
-        create_dnpy_files(vol, filename=filename)
+        #create_dnpy_files(vol, filename=filename)
+        create_dnpy_files(da_vol.tondarray(), filename=filename)
     t1 = time()
     dt = t1 - t0
     print('Creation time: %.3f sec' % (dt))
