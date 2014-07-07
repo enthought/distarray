9a05d5f3927d967d9a371b00d14608ac7358847a
Kurt Smith <ksmith@enthought.com>
2014-07-07 15:57:32 -0500

    Some tweaks to setup.parse_readme()

--- setup.py
+++ setup.py
@@ -7,49 +7,58 @@
 from setuptools import setup, find_packages
 
 
-def parse_readme(filename='README.rst', skiplines=6):
-    """Return file `filename` as a string, skipping some lines."""
+def parse_readme(filename='README.rst', sentinel="README"):
+    """
+    Return file `filename` as a string.
+
+    Skips lines until it finds a comment that contains `sentinel` text.  This
+    effectively strips off any badges or other undesirable content.
+    """
     with open(filename, 'r') as fp:
-        for line in range(skiplines):
-            fp.readline()
-        return fp.read()
+        lines = fp.readlines()
+    for idx, line in enumerate(lines):
+        if line.startswith('..') and sentinel in line:
+            break
+    return "".join(lines[idx+1:])
 
 
-install_requires = [
-    'ipython',
-    'numpy',
-    'mpi4py'
-]
+if __name__ == "__main__":
 
-metadata = {
-    'name': 'distarray',
-    'version': '0.4.0-dev',
-    'description': 'Distributed Memory Arrays for Python',
-    'keywords': 'parallel mpi distributed array',
-    'license': 'New BSD',
-    'author': 'IPython Development Team and Enthought, Inc.',
-    'author_email': 'ksmith@enthought.com',
-    'url': 'https://github.com/enthought/distarray',
-    'packages': find_packages(),
-    'install_requires': install_requires,
-    'long_description': parse_readme(),
-    'platforms': ["Linux", "Mac OS-X"],
-    'entry_points': {'console_scripts': ['dacluster = '
-                                         'distarray.apps.dacluster:main']},
-    'classifiers': [c.strip() for c in """\
-        Development Status :: 2 - Pre-Alpha
-        Intended Audience :: Developers
-        Intended Audience :: Science/Research
-        License :: OSI Approved :: BSD License
-        Operating System :: MacOS
-        Operating System :: OS Independent
-        Operating System :: POSIX
-        Operating System :: Unix
-        Programming Language :: Python
-        Topic :: Scientific/Engineering
-        Topic :: Software Development
-        Topic :: Software Development :: Libraries
-        """.splitlines() if len(c.strip()) > 0],
-}
+    install_requires = [
+        'ipython',
+        'numpy',
+        'mpi4py'
+    ]
 
-setup(**metadata)
+    metadata = {
+        'name': 'distarray',
+        'version': '0.4.0-dev',
+        'description': 'Distributed Memory Arrays for Python',
+        'keywords': 'parallel mpi distributed array',
+        'license': 'New BSD',
+        'author': 'IPython Development Team and Enthought, Inc.',
+        'author_email': 'ksmith@enthought.com',
+        'url': 'https://github.com/enthought/distarray',
+        'packages': find_packages(),
+        'install_requires': install_requires,
+        'long_description': parse_readme(),
+        'platforms': ["Linux", "Mac OS-X"],
+        'entry_points': {'console_scripts': ['dacluster = '
+                                            'distarray.apps.dacluster:main']},
+        'classifiers': [c.strip() for c in """\
+            Development Status :: 2 - Pre-Alpha
+            Intended Audience :: Developers
+            Intended Audience :: Science/Research
+            License :: OSI Approved :: BSD License
+            Operating System :: MacOS
+            Operating System :: OS Independent
+            Operating System :: POSIX
+            Operating System :: Unix
+            Programming Language :: Python
+            Topic :: Scientific/Engineering
+            Topic :: Software Development
+            Topic :: Software Development :: Libraries
+            """.splitlines() if len(c.strip()) > 0],
+    }
+
+    setup(**metadata)
