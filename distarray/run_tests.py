a703d11b6f5384a0a5bae720d1fe1f12a3d597e3
Robert David Grant <rgrant@enthought.com>
2014-07-17 17:57:18 -0500

    Rename test_runner to run_tests.py.

--- distarray/run_tests.py
+++ distarray/run_tests.py
@@ -12,15 +12,16 @@ from __future__ import print_function
 
 import unittest
 import os
+import sys
 import shlex
 from subprocess import Popen, PIPE
 
 import distarray
 
 
-def run_ipython_parallel_tests():
+def run_ipython_parallel_tests(path):
     """Run ipython-parallel client tests."""
-    test_suite = unittest.defaultTestLoader.discover('.')
+    test_suite = unittest.defaultTestLoader.discover(path)
     return unittest.TextTestRunner().run(test_suite)
 
 
@@ -28,42 +29,45 @@ def _process_shell_output(specific_cmd):
     """Run a command with subprocess and pass the results through to stderr."""
     proc = Popen(shlex.split(specific_cmd), stdout=PIPE, stderr=PIPE)
     while True:
-        char = proc.stderr.read(1).decode()
+        char = proc.stderr.read().decode()
         if not char:
             break
         else:
-            print(char, end="", flush=True)
+            print(char, end="")
+            sys.stderr.flush()
 
 
-def run_mpi_only_tests(nengines=4):
+def run_mpi_only_tests(path, nengines=4):
     """Run mpi-client tests."""
-    path = os.path.split(distarray.__file__)[0]
     cmd = ("mpiexec "
-           "-np 1 python -m unittest discover -c : "
-           "-np {nengines} {path}/apps/engine.py")
+           "-np 1 python -m unittest discover -c -s {path} : "
+           "-np {nengines} {path}/distarray/apps/engine.py")
     specific_cmd = cmd.format(nengines=nengines, path=path)
+    print(specific_cmd)
     _process_shell_output(specific_cmd)
 
 
-def run_local_tests(nengines=4):
+def run_local_tests(path, nengines=4):
     """Run engine-only MPI tests."""
     cmd = ("mpiexec "
            "-np {nengines} python -m unittest discover -cp "
-           "paralleltest_*.py")
-    specific_cmd = cmd.format(nengines=nengines)
+           "paralleltest_*.py -s {path}")
+    specific_cmd = cmd.format(nengines=nengines, path=path)
+    print(specific_cmd)
     _process_shell_output(specific_cmd)
 
 
 def test(nengines=4):
     """Run all DistArray tests."""
+    path = os.path.split(os.path.split(distarray.__file__)[0])[0]
     print("IPython.parallel client tests:")
-    run_ipython_parallel_tests()
+    run_ipython_parallel_tests(path)
     print()
     print("MPI client tests:")
-    run_mpi_only_tests(nengines=nengines)
+    run_mpi_only_tests(path, nengines=nengines)
     print()
     print("Engine-only MPI tests:")
-    run_local_tests(nengines=nengines)
+    run_local_tests(path, nengines=nengines)
 
 
 if __name__ == "__main__":
