18b3a1260f2b577f8bfc2c2af14978d1fb1bdcc1
Mark Kness <mkness@enthought.com>
2014-07-17 14:40:23 -0500

    Docstring for exception classes.

--- distarray/error.py
+++ distarray/error.py
@@ -3,34 +3,42 @@
 #  Copyright (C) 2008-2014, IPython Development Team and Enthought, Inc.
 #  Distributed under the terms of the BSD License.  See COPYING.rst.
 # ---------------------------------------------------------------------------
+
 """
-Define error classes.
+Exception classes for DistArray errors.
 """
 
 
 class DistArrayError(Exception):
+    """ Base exception class for DistArray errors. """
     pass
 
 
 class MPIDistArrayError(DistArrayError):
+    """ Base exception class for MPI distribution errors. """
     pass
 
 
 class ContextError(DistArrayError):
+    """ Exception class when a unique Context cannot be found. """
     pass
 
 
 class InvalidCommSizeError(MPIDistArrayError):
+    """ Exception class when a requested communicator is too large. """
     pass
 
 
 class InvalidRankError(MPIDistArrayError):
+    """ Exception class when an invalid rank is used in a communicator. """
     pass
 
 
 class MPICommError(MPIDistArrayError):
+    # This seems to be unused.
     pass
 
 
 class DistributionError(DistArrayError):
+    """ Exception class when inconsistent distributions are used. """
     pass
