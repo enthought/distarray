4e531ea3bc7abc335f9491603a34e85087cfb274
Robert David Grant <rgrant@enthought.com>
2014-07-18 11:56:39 -0500

    Simplify run_tests quite a lot.
    Remove the ability to run specific tests individually, but this probably isn't needed here.

--- distarray/run_tests.py
+++ distarray/run_tests.py
@@ -10,64 +10,37 @@ Functions for running DistArray tests.
 
 from __future__ import print_function
 
-import unittest
 import os
 import sys
 import shlex
-from subprocess import Popen, PIPE
+import subprocess
 
 import distarray
 
 
-def run_ipython_parallel_tests(path):
-    """Run ipython-parallel client tests."""
-    test_suite = unittest.defaultTestLoader.discover(path)
-    return unittest.TextTestRunner().run(test_suite)
+def _run_shell_command(specific_cmd):
+    """Run a command with subprocess and pass the results through to stdout.
 
-
-def _process_shell_output(specific_cmd):
-    """Run a command with subprocess and pass the results through to stderr."""
-    proc = Popen(shlex.split(specific_cmd), stdout=PIPE, stderr=PIPE)
+    First, change directory to the project directory.
+    """
+    path = os.path.split(os.path.split(distarray.__file__)[0])[0]
+    os.chdir(path)
+    proc = subprocess.Popen(shlex.split(specific_cmd),
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT)
     while True:
-        char = proc.stderr.read().decode()
+        char = proc.stdout.read(1).decode()
         if not char:
             break
         else:
             print(char, end="")
-            sys.stderr.flush()
+            sys.stdout.flush()
 
 
-def run_mpi_only_tests(path, nengines=4):
-    """Run mpi-client tests."""
-    cmd = ("mpiexec "
-           "-np 1 python -m unittest discover -c -s {path} : "
-           "-np {nengines} {path}/distarray/apps/engine.py")
-    specific_cmd = cmd.format(nengines=nengines, path=path)
-    print(specific_cmd)
-    _process_shell_output(specific_cmd)
-
-
-def run_local_tests(path, nengines=4):
-    """Run engine-only MPI tests."""
-    cmd = ("mpiexec "
-           "-np {nengines} python -m unittest discover -cp "
-           "paralleltest_*.py -s {path}")
-    specific_cmd = cmd.format(nengines=nengines, path=path)
-    print(specific_cmd)
-    _process_shell_output(specific_cmd)
-
-
-def test(nengines=4):
+def test():
     """Run all DistArray tests."""
-    path = os.path.split(os.path.split(distarray.__file__)[0])[0]
-    print("IPython.parallel client tests:")
-    run_ipython_parallel_tests(path)
-    print()
-    print("MPI client tests:")
-    run_mpi_only_tests(path, nengines=nengines)
-    print()
-    print("Engine-only MPI tests:")
-    run_local_tests(path, nengines=nengines)
+    cmd = "make test"
+    _run_shell_command(cmd)
 
 
 if __name__ == "__main__":
