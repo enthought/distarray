6e43255a636be4f587d62c9706e81774dd941cad
Robert David Grant <rgrant@enthought.com>
2014-09-03 00:14:49 -0500

    Improve benchmark.

--- examples/lazy_eval/benchmark_lazy_eval.py
+++ examples/lazy_eval/benchmark_lazy_eval.py
@@ -11,23 +11,45 @@ evaluation.
 
 """
 
-from distarray.globalapi import Context, tanh
-from timeit import default_timer as time
+from __future__ import print_function, division
 
-nops = 10000
-arr_shape = (100, 100)
+from sys import stderr
+from timeit import default_timer as time
+from pprint import pprint
+
+from distarray.globalapi import Context, tanh
+
+
+nops_list = range(1, 10002, 1000)
+arr_shape = (10, 10)
+arr_size = arr_shape[0] * arr_shape[1]
 
 context = Context()
 
-arr = context.ones(arr_shape)
-start = time()
-for _ in range(nops):
-    arr = tanh(arr)
-print("Eager time: {:0.2f}".format(time() - start))
+eager = []
+lazy = []
 
-arr = context.ones(arr_shape)
-start = time()
-with context.lazy_eval():
-    for i in range(nops):
+for nops in nops_list:
+
+    arr = context.ones(arr_shape)
+    start = time()
+    for _ in range(nops):
         arr = tanh(arr)
-print("Lazy time: {:0.2f}".format(time() - start))
+    result = time() - start
+    eager.append(result)
+    print('.', end='', file=stderr, flush=True)
+
+    arr = context.ones(arr_shape)
+    start = time()
+    with context.lazy_eval():
+        for i in range(nops):
+            arr = tanh(arr)
+    result = time() - start
+    lazy.append(result)
+    print('.', end='', file=stderr, flush=True)
+
+print(file=stderr, flush=True)
+pprint({"nops": list(nops_list),
+        "Eager": eager,
+        "Lazy": lazy,
+       }, stream=stderr)
