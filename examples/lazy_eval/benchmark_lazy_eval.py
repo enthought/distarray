a34b002ca17f58b237565646e15c19a88702b8eb
Robert David Grant <rgrant@enthought.com>
2014-09-04 15:31:42 -0500

    Make into functions.

--- examples/lazy_eval/benchmark_lazy_eval.py
+++ examples/lazy_eval/benchmark_lazy_eval.py
@@ -21,61 +21,70 @@ import numpy
 from distarray.globalapi import Context, tanh
 
 
-nops_list = range(1, 20002, 1000)
-arr_shape = (10, 10)
-arr_size = arr_shape[0] * arr_shape[1]
+def benchmark(nops_list, arr_shape):
+    context = Context()
 
-context = Context()
+    data = {"nops": list(nops_list),
+            "Numpy": [],
+            "Eager": [],
+            "Lazy": [],
+            }
 
-numpy_times = []
-eager_times = []
-lazy_times = []
+    total_tests = len(nops_list) * 3
+    test_num = 1
+    for nops in nops_list:
 
-data = {"nops": list(nops_list),
-        "Numpy": numpy_times,
-        "Eager": eager_times,
-        "Lazy": lazy_times,
-       }
+        # bench Numpy
+        arr = numpy.ones(arr_shape)
+        start = time()
+        for _ in range(nops):
+            arr = numpy.tanh(arr)
+        result = time() - start
+        data['Numpy'].append(result)
+        print('({}/{}), Numpy, {} ops, {:0.3f} s'.format(test_num, total_tests, nops, result),
+            file=stderr, flush=True)
+        test_num += 1
 
-total_tests = len(nops_list) * 3
-test_num = 1
-for nops in nops_list:
-
-    # bench Numpy
-    arr = numpy.ones(arr_shape)
-    start = time()
-    for _ in range(nops):
-        arr = numpy.tanh(arr)
-    result = time() - start
-    numpy_times.append(result)
-    print('({}/{}), Numpy, {} ops, {:0.3f} s'.format(test_num, total_tests, nops, result),
-          file=stderr, flush=True)
-    test_num += 1
-
-    # bench DistArray eager eval
-    arr = context.ones(arr_shape)
-    start = time()
-    for _ in range(nops):
-        arr = tanh(arr)
-    result = time() - start
-    eager_times.append(result)
-    print('({}/{}), Eager, {} ops, {:0.3f} s'.format(test_num, total_tests, nops, result),
-          file=stderr, flush=True)
-    test_num += 1
-
-    # bench DistArray lazy eval
-    arr = context.ones(arr_shape)
-    start = time()
-    with context.lazy_eval():
-        for i in range(nops):
+        # bench DistArray eager eval
+        arr = context.ones(arr_shape)
+        start = time()
+        for _ in range(nops):
             arr = tanh(arr)
-    result = time() - start
-    lazy_times.append(result)
-    print('({}/{}),  Lazy, {} ops, {:0.3f} s'.format(test_num, total_tests, nops, result),
-          file=stderr, flush=True)
-    test_num += 1
+        result = time() - start
+        data['Eager'].append(result)
+        print('({}/{}), Eager, {} ops, {:0.3f} s'.format(test_num, total_tests, nops, result),
+            file=stderr, flush=True)
+        test_num += 1
 
-now = datetime.datetime.now()
-filename = '_'.join((now.strftime("%Y-%m-%dT%H-%M-%S"), str(nops_list[-1]))) + ".json"
-with open(filename, 'w') as fp:
-    json.dump(data, fp, indent=4)
+        # bench DistArray lazy eval
+        arr = context.ones(arr_shape)
+        start = time()
+        with context.lazy_eval():
+            for i in range(nops):
+                arr = tanh(arr)
+        result = time() - start
+        data['Lazy'].append(result)
+        print('({}/{}),  Lazy, {} ops, {:0.3f} s'.format(test_num, total_tests, nops, result),
+            file=stderr, flush=True)
+        test_num += 1
+
+    return data
+
+
+def save_data(data, note=''):
+    now = datetime.datetime.now()
+    filename = '_'.join((now.strftime("%Y-%m-%dT%H-%M-%S"), note)) + ".json"
+    with open(filename, 'w') as fp:
+        json.dump(data, fp, indent=4)
+
+
+def main(nops_list=None, arr_shape=None, note=''):
+    nops_list = range(1, 20002, 1000) if nops_list is None else nops_list
+    arr_shape = (10, 10) if arr_shape is None else arr_shape
+    data = benchmark(nops_list, arr_shape)
+    save_data(data, note)
+
+    return data
+
+if __name__ == '__main__':
+    main()
