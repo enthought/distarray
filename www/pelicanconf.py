3dc9204c80f2f7be5f82200c059a6a62f02bf6c1
Robert David Grant <rgrant@enthought.com>
2014-09-05 11:28:58 -0500

    Update blogroll and social links.

--- www/pelicanconf.py
+++ www/pelicanconf.py
@@ -18,14 +18,14 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('NumPy', 'http://www.numpy.org/'),
+         ('SciPy', 'http://www.scipy.org'),
+         ('IPython', 'http://ipython.org/'),
+         ('Enthought', 'http://www.enthought.com/'),
+        )
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('github', 'https://github.com/enthought/distarray'),)
 
 DEFAULT_PAGINATION = False
 
