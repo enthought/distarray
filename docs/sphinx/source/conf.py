b346979475bc69cb507ca0a6b3bab72af6555e6a
Robert David Grant <rgrant@enthought.com>
2015-07-08 15:32:05 -0500

    Update sphinxcontrib.napoleon->sphinx.ext.napoleon
    Requires Sphinx > 1.3

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -46,12 +46,12 @@ sys.path.insert(0, os.path.abspath('../../distarray'))
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
+needs_sphinx = '1.3'
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.mathjax',
-              'sphinx.ext.autosummary', 'sphinxcontrib.napoleon',
+              'sphinx.ext.autosummary', 'sphinx.ext.napoleon',
               'sphinxcontrib.programoutput']
 
 # Add any paths that contain templates here, relative to this directory.
